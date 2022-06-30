dd45907a6fa3b20fe17bf36e387537ca500dda4e
Donggyu <a29661498@gmail.com>
2022-06-30 16:18:37 +0900

    chore: 프로젝트 생성

---
+++ backend/src/main/java/com/woowacourse/moamoa/MoamoaApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.moamoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoamoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoamoaApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/woowacourse/moamoa/MoamoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.moamoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoamoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
