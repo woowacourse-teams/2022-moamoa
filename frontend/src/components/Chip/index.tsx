d60a9a3e2e550d6fe955655b55f3f11f67f0bb1a
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-07-04 20:20:28 +0900

    refactor: Chip 스타일 파일 분리
    Co-Authored-By: airman5573 <68623798+airman5573@users.noreply.github.com>

--- frontend/src/components/Chip/index.tsx
+++ frontend/src/components/Chip/index.tsx
@@ -1,27 +1,16 @@
-import { css } from '@emotion/react';
+import { MakeOptional } from '@custom-types/index';
+
+import { StyledChip } from './style';
 
 export interface ChipProps {
   children: string;
   disabled: boolean;
 }
 
-const Chip: React.FC<ChipProps> = ({ children, disabled = false }) => {
-  return (
-    <span
-      css={theme => css`
-        display: inline-block;
-        width: 3.75rem;
-        padding: 0.5rem 0.8rem;
+export type OptionalChipProps = MakeOptional<ChipProps, 'disabled'>;
 
-        text-align: center;
-        border-radius: 1rem;
-        color: ${disabled ? theme.colors.black : theme.colors.white};
-        background-color: ${disabled ? theme.colors.secondary.base : theme.colors.primary.base};
-      `}
-    >
-      {children}
-    </span>
-  );
+const Chip: React.FC<OptionalChipProps> = ({ children, disabled = false }) => {
+  return <StyledChip disabled={disabled}>{children}</StyledChip>;
 };
 
 export default Chip;
---
+++ frontend/src/components/Chip/style.ts
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+import type { ChipProps } from '@components/Chip';
+
+export const StyledChip = styled.span<ChipProps>`
+  ${({ theme, disabled }) => `
+    display: inline-block;
+
+    width: 3.75rem;
+    padding: 0.5rem 0.8rem;
+    
+    text-align: center;
+    border-radius: 1rem;
+    color: ${disabled ? theme.colors.black : theme.colors.white};
+    background-color: ${disabled ? theme.colors.secondary.base : theme.colors.primary.base};
+  `}
+`;
