d1c6fe9ba047fbb908a46b48f3c6984960ff0e82
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-07-04 16:28:25 +0900

    feat: theme 적용
    emotion ThemeProvider 적용
    
    Co-Authored-By: airman5573 <68623798+airman5573@users.noreply.github.com>

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,24 @@
+import { ThemeProvider } from '@emotion/react';
+import React from 'react';
+
+import { theme } from '@styles/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story, context) => {
+    return (
+      <ThemeProvider theme={theme}>
+        <Story {...context} />
+      </ThemeProvider>
+    );
+  },
+];
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,18 @@
+import { ThemeProvider } from '@emotion/react';
 import { createRoot } from 'react-dom/client';
+
+import { theme } from '@styles/theme';
 
 import App from './App';
 
 const $root = document.getElementById('root');
 if ($root) {
   const root = createRoot($root);
-  root.render(<App />);
+  root.render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+  );
 } else {
   throw new Error('root element is not exist');
 }
