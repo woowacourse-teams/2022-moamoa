344ffb5bdccc6d4bdf8fda9cd0d30ae6c65d41e7
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-07-05 14:04:38 +0900

    fix: Chip 로직 수정

--- frontend/src/components/StudyChip/index.tsx
+++ frontend/src/components/StudyChip/index.tsx
@@ -1,7 +1,7 @@
 import Chip from '@components/Chip';
 
 const StudyChip = ({ isOpen }: { isOpen: boolean }) => {
-  return <Chip disabled={isOpen}>{isOpen ? '모집중' : '모집완료'}</Chip>;
+  return <Chip disabled={!isOpen}>{isOpen ? '모집중' : '모집완료'}</Chip>;
 };
 
 export default StudyChip;
