06e78b2e2280f8ce92d3297af0f86fb0dd787375
SeungCheol Shin <47477359+sc0116@users.noreply.github.com>
2022-07-07 11:29:43 +0900

    feat: cors 허용 (#29)

--- backend/src/test/java/com/woowacourse/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/acceptance/AcceptanceTest.java
@@ -1,12 +1,16 @@
 package com.woowacourse.acceptance;
 
-import com.woowacourse.moamoa.MoamoaApplication;
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
+
+import com.woowacourse.moamoa.MoamoaApplication;
+
+import io.restassured.RestAssured;
 
 @SpringBootTest(
         webEnvironment = WebEnvironment.RANDOM_PORT,
@@ -20,5 +24,17 @@ public class AcceptanceTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+    }
+
+    @DisplayName("cors 적용 여부 확인")
+    @Test
+    void corsTest() {
+        RestAssured.given()
+            .header("Origin", "https://xxx.com")
+            .header("Access-Control-Request-Method", "GET")
+            .when()
+            .options("/api/studies")
+            .then()
+            .statusCode(200);
     }
 }
