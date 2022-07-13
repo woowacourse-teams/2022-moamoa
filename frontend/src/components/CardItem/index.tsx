7d154e717bf2c8c2f7769739d3a65a1d335d1b07
airman5573 <rpf5573@kakao.com>
2022-07-13 13:50:17 +0900

    setting: 불필요한 설정 삭제 및 소소한 수정

--- frontend/src/components/CardItem/index.tsx
+++
@@ -1,7 +0,0 @@
-const Card = () => {
-  return <div>
-
-  </div>;
-};
-
-export default Card;
--- frontend/src/components/Logo/Logo.stories.tsx
+++ frontend/src/components/Logo/Logo.stories.tsx
@@ -1,15 +1,13 @@
 import { Story } from '@storybook/react';
 
-import Logo from "@components/Logo";
+import Logo from '@components/Logo';
 
 export default {
   title: 'Components/Logo',
   component: Logo,
 };
 
-const Template: Story = props => (
-  <Logo {...props} />
-);
+const Template: Story = props => <Logo {...props} />;
 
 export const Default = Template.bind({});
 Default.args = {};
