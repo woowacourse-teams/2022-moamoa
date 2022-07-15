5599b579b2d376dafa95c35cac11e953333e2be5
Donggyu <a29661498@gmail.com>
2022-07-15 14:02:05 +0900

    feat: DDL 작성 및 수정

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,8 +1,9 @@
-DROP TABLE IF EXISTS study_tag;
+DROP TABLE IF EXISTS study_filter;
 DROP TABLE IF EXISTS study_member;
-DROP TABLE IF EXISTS study;
-DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS filter;
+DROP TABLE IF EXISTS category;
 DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS study;
 
 CREATE TABLE study
 (
@@ -20,19 +21,27 @@ CREATE TABLE study
     owner VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE tag
+CREATE TABLE category
 (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    tag_name VARCHAR(255) NOT NULL
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE study_tag
+CREATE TABLE filter
+(
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    category_id BIGINT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE study_filter
 (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     study_id BIGINT,
-    tag_id BIGINT,
+    filter_id BIGINT,
     FOREIGN KEY (study_id) REFERENCES study (id),
-    FOREIGN KEY (tag_id) REFERENCES tag (id)
+    FOREIGN KEY (filter_id) REFERENCES filter (id)
 );
 
 CREATE TABLE member
