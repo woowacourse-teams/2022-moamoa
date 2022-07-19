f49328740d549fb566000d3d5c449e561b8741f1
airman5573 <rpf5573@kakao.com>
2022-07-19 13:40:59 +0900

    feat: 날짜 포멧 변환 함수 추가

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,4 +1,5 @@
 import { mqBetween, mqDown, mqUp } from '@utils/media-query';
 import noop from '@utils/noop';
+import yyyymmddTommdd from '@utils/yyyymmddTommdd';
 
-export { mqDown, mqUp, mqBetween, noop };
+export { mqDown, mqUp, mqBetween, noop, yyyymmddTommdd };
