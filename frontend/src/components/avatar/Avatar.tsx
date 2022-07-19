66a31ed1fbbba430bacb01d8765dafd3abc290a3
airman5573 <rpf5573@kakao.com>
2022-07-19 15:17:04 +0900

    fix: 폴더의 대소문자를 구분하지 않는다

---
+++ frontend/src/components/avatar/Avatar.tsx
@@ -0,0 +1,22 @@
+import { MakeOptional } from '@custom-types/index';
+
+import * as S from './Avatar.style';
+
+export type AvatarProps = {
+  className?: string;
+  profileImg: string;
+  profileAlt: string;
+  size: 'sm' | 'md' | 'lg';
+};
+
+type OptionalAvatarProps = MakeOptional<AvatarProps, 'size'>;
+
+const Avatar: React.FC<OptionalAvatarProps> = ({ className, size = 'sm', profileImg, profileAlt }) => {
+  return (
+    <S.Avatar className={className} size={size}>
+      <S.Image src={profileImg} alt={profileAlt} />
+    </S.Avatar>
+  );
+};
+
+export default Avatar;
---
+++ frontend/src/components/card/Card.stories.tsx
@@ -0,0 +1,37 @@
+import { Story } from '@storybook/react';
+
+import type { CardProps } from '@components/card/Card';
+import Card from '@components/card/Card';
+import Chip from '@components/chip/Chip';
+
+export default {
+  title: 'Components/Card',
+  component: Card,
+  argTypes: {
+    thumbnailUrl: { controls: 'text' },
+    thumbnailAlt: { controls: 'text' },
+    title: { controls: 'text' },
+    description: { controls: 'text' },
+    extraChips: { controls: 'object' },
+  },
+};
+
+const Template: Story<CardProps> = props => (
+  <div style={{ width: '256px' }}>
+    <Card {...props} />
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  thumbnailUrl:
+    'https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1673&q=80',
+  thumbnailAlt: '이미지 Alt',
+  title: '타이틀',
+  description: '세부 설명',
+  extraChips: [
+    <Chip disabled={false} key="1">
+      Chip
+    </Chip>,
+  ],
+};
