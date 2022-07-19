1c1e9b8afdbb578959351c2780f46aa29c4dad00
Donggyu <a29661498@gmail.com>
2022-07-20 08:02:47 +0900

    [BE] issue60: Filter로 필터링하여 스터디 목록 조회 (#90)
    * feat: DDL 작성 및 수정
    
    * feat: build.gradle 의존성 추가
    
    * feat: 기존 테스트 제거
    
    * feat: StudyFilter 생성 및 네이밍 변경
    
    * feat: 카테고리별로 필터링 해서 검색하기 구현
    
    * feat: Service 및 Controller 구현
    
    * refactor: 패키지 구조 개선
    
    * feat: FilterNotExistException 추가
    
    * feat: 사용하지 않는 메소드 제거
    
    * test: 테스트 케이스 추가
    
    * refactor: Filter -> Tag 로 네이밍 변경
    
    * fix: QueryDSL Q타입 객체 수정
    
    * fix: DDL 수정
    
    * refactor: JPAQueryFactory 빈 등록 제거
    
    * fix: schema DROP 문 수정

---
+++ backend/src/main/java/com/woowacourse/moamoa/study/domain/study/Study.java
@@ -0,0 +1,61 @@
+package com.woowacourse.moamoa.study.domain.study;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.woowacourse.moamoa.study.domain.studytag.StudyTag;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Study {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String title;
+    private String excerpt;
+    private String thumbnail;
+    private String status;
+
+    @OneToMany(mappedBy = "study")
+    private List<StudyTag> studyTags = new ArrayList<>();
+
+    public Study(final Long id, final String title, final String excerpt,
+                 final String thumbnail, final String status
+    ) {
+        this.id = id;
+        this.title = title;
+        this.excerpt = excerpt;
+        this.thumbnail = thumbnail;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Study study = (Study) o;
+        return Objects.equals(id, study.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ backend/src/main/java/com/woowacourse/moamoa/tag/domain/Category.java
@@ -0,0 +1,34 @@
+package com.woowacourse.moamoa.tag.domain;
+
+import static lombok.AccessLevel.PROTECTED;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+public class Category {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name = "filter_id")
+    private List<Tag> tags = new ArrayList<>();
+
+    public Category(final Long id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
