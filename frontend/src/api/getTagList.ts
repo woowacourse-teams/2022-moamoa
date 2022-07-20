09aeb8f5e9b7b278b96ad5eed238acdbf524c209
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-07-20 16:39:56 +0900

    refactor: 콘솔 제거

--- frontend/src/api/getTagList.ts
+++ frontend/src/api/getTagList.ts
@@ -2,6 +2,5 @@ import axiosInstance from '@api/axiosInstance';
 
 export const getTagList = async () => {
   const response = await axiosInstance.get(`/api/tags`);
-  console.log(response.data);
   return response.data;
 };
