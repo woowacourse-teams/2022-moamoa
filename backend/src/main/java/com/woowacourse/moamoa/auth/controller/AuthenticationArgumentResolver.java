70a5429af20e70fc51c5116a2a13e7603fff920d
jaejae-yoo <wotj102@gmail.com>
2022-07-24 13:58:05 +0900

    refactor: Authorization 인증 타입 검증 로직 추가

--- backend/src/main/java/com/woowacourse/moamoa/auth/controller/AuthenticationArgumentResolver.java
+++ backend/src/main/java/com/woowacourse/moamoa/auth/controller/AuthenticationArgumentResolver.java
@@ -3,6 +3,7 @@ package com.woowacourse.moamoa.auth.controller;
 import com.woowacourse.moamoa.auth.config.AuthenticationExtractor;
 import com.woowacourse.moamoa.auth.config.AuthenticationPrincipal;
 import com.woowacourse.moamoa.auth.infrastructure.TokenProvider;
+import com.woowacourse.moamoa.common.exception.UnauthorizedException;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
@@ -29,6 +30,10 @@ public class AuthenticationArgumentResolver implements HandlerMethodArgumentReso
         final HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
 
         final String token = AuthenticationExtractor.extract(request);
+        if (token == null) {
+            throw new UnauthorizedException("인증 타입이 올바르지 않습니다.");
+        }
+
         return Long.valueOf(tokenProvider.getPayload(token));
     }
 }
