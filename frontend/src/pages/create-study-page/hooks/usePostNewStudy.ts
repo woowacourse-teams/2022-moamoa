57c2f5dae5912b233e183a2ff441d2fcaefe1ce2
airman5573 <rpf5573@kakao.com>
2022-07-26 14:50:58 +0900

    refactor: useMutation에 key 제거

--- frontend/src/pages/create-study-page/hooks/usePostNewStudy.ts
+++ frontend/src/pages/create-study-page/hooks/usePostNewStudy.ts
@@ -3,7 +3,7 @@ import { useMutation } from 'react-query';
 import postNewStudy from '@api/postNewStudy';
 
 const usePostNewStudy = () => {
-  return useMutation('post-new-study', postNewStudy);
+  return useMutation(postNewStudy);
 };
 
 export default usePostNewStudy;
