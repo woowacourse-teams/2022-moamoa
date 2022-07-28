94dcdf4366372ed51659eb650193d483dae00bfb
SeungCheol <ssc6839@gmail.com>
2022-07-28 10:49:35 +0900

    style: 상수명 대문자로 변경

--- backend/src/main/java/com/woowacourse/moamoa/review/domain/exception/InvalidReviewException.java
+++ backend/src/main/java/com/woowacourse/moamoa/review/domain/exception/InvalidReviewException.java
@@ -4,10 +4,10 @@ import com.woowacourse.moamoa.common.exception.BadRequestException;
 
 public class InvalidReviewException extends BadRequestException {
 
-    private static final String message = "스터디 시작 전 후기를 작성할 수 없습니다.";
+    private static final String MESSAGE = "스터디 시작 전 후기를 작성할 수 없습니다.";
 
     public InvalidReviewException() {
-        super(message);
+        super(MESSAGE);
     }
 
     public InvalidReviewException(final String message) {
