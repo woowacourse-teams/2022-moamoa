6393afa11601fc7f78e1925d2ba87e0613d87671
jaejae-yoo <wotj102@gmail.com>
2022-07-30 21:08:04 +0900

    feat: 사용자 인증 요청 처리를 위한 빌더 생성

---
+++ backend/src/main/java/com/woowacourse/moamoa/auth/controller/matcher/AuthenticationRequestMatcherBuilder.java
@@ -0,0 +1,28 @@
+package com.woowacourse.moamoa.auth.controller.matcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.springframework.http.HttpMethod;
+
+public class AuthenticationRequestMatcherBuilder {
+
+    private static final List<HttpMethod> METHODS = List.of(
+            HttpMethod.GET, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PUT, HttpMethod.PATCH
+    );
+
+    private final Map<HttpMethod, List<String>> authenticationPaths;
+
+
+    public AuthenticationRequestMatcherBuilder() {
+        this.authenticationPaths = METHODS.stream()
+                .collect(Collectors.toMap(method -> method, method -> new ArrayList<>()));
+    }
+
+    public AuthenticationRequestMatcherBuilder setUpAuthenticationPath(HttpMethod method, String... urls) {
+        final List<String> paths = authenticationPaths.get(method);
+        paths.addAll(List.of(urls));
+        return this;
+    }
+}
