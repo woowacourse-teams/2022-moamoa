7f37285bbae815c78632c69702b7cddc5e6515d9
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-07-30 17:48:32 +0900

    style: DropDownBox 스타일 수정

--- frontend/src/layout/header/components/drop-down-box/DropDownBox.style.tsx
+++ frontend/src/layout/header/components/drop-down-box/DropDownBox.style.tsx
@@ -26,5 +26,16 @@ export const DropDownBox = styled.div<Pick<DropDownBoxProps, 'top' | 'bottom' | 
     border-radius: 5px;
     background-color: ${theme.colors.secondary.light};
     z-index: 3;
+
+    transform-origin: top;
+    animation: slidein 0.1s ease;
+    @keyframes slidein {
+      0% {
+        transform: scale(1, 0);
+      }
+      100% {
+        transform: scale(1, 1);
+      }
+    }
   `}
 `;
