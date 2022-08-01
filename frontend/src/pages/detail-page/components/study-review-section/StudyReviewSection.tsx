c043531894b2a8e4c69ca052246a0e58841ef22c
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-01 15:56:41 +0900

    fix: 후기 0개 오류 수정

--- frontend/src/pages/detail-page/components/study-review-section/StudyReviewSection.tsx
+++ frontend/src/pages/detail-page/components/study-review-section/StudyReviewSection.tsx
@@ -15,43 +15,48 @@ const StudyReviewSection: React.FC<StudyReviewSectionProps> = ({ studyId }) => {
   const [isMoreButtonVisible, setIsMoreButtonVisible] = useState<boolean>(true);
   const showAll = !isMoreButtonVisible;
   const size = showAll ? undefined : DEFAULT_LOAD_STUDY_REVIEW_COUNT;
-  const { data, isError, isFetching } = useFetchStudyReviews(Number(studyId), size);
+  const { data, isError, isFetching, isSuccess } = useFetchStudyReviews(Number(studyId), size);
 
   const handleMoreButtonClick = () => {
     setIsMoreButtonVisible(prev => !prev);
   };
 
+  const renderReviews = () => {
+    if (isFetching) return <div>로딩중...</div>;
+    if (!isSuccess || isError) return <div>후기 불러오기를 실패했습니다</div>;
+    if (data.reviews.length === 0) return <div>아직 작성된 후기가 없습니다</div>;
+    return (
+      <>
+        <S.ReviewList>
+          {data.reviews.map(review => (
+            <S.ReviewListItem key={review.id}>
+              <StudyReviewCard
+                imageUrl={review.member.imageUrl}
+                username={review.member.username}
+                reviewDate={review.createdAt}
+                review={review.content}
+              />
+            </S.ReviewListItem>
+          ))}
+        </S.ReviewList>
+        {data.reviews.length >= DEFAULT_LOAD_STUDY_REVIEW_COUNT && (
+          <S.MoreButtonContainer>
+            <MoreButton
+              status={showAll ? 'unfold' : 'fold'}
+              onClick={handleMoreButtonClick}
+              foldText="- 접기"
+              unfoldText="+ 더보기"
+            />
+          </S.MoreButtonContainer>
+        )}
+      </>
+    );
+  };
+
   return (
     <S.ReviewSection>
       <S.ReviewTitle>후기 {data?.totalResults && <span>{data.totalResults}개</span>}</S.ReviewTitle>
-      {isFetching && <div>로딩중...</div>}
-      {isError && <div>후기 불러오기를 실패했습니다.</div>}
-      {data?.reviews && (
-        <>
-          <S.ReviewList>
-            {data.reviews.map(review => (
-              <S.ReviewListItem key={review.id}>
-                <StudyReviewCard
-                  imageUrl={review.member.imageUrl}
-                  username={review.member.username}
-                  reviewDate={review.createdAt}
-                  review={review.content}
-                />
-              </S.ReviewListItem>
-            ))}
-          </S.ReviewList>
-          {data.reviews.length && (
-            <S.MoreButtonContainer>
-              <MoreButton
-                status={showAll ? 'unfold' : 'fold'}
-                onClick={handleMoreButtonClick}
-                foldText="- 접기"
-                unfoldText="+ 더보기"
-              />
-            </S.MoreButtonContainer>
-          )}
-        </>
-      )}
+      {renderReviews()}
     </S.ReviewSection>
   );
 };
