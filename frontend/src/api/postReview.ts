4618417bd56aca339a7cca42d11684b96f7abe88
airman5573 <rpf5573@kakao.com>
2022-08-01 18:12:22 +0900

    fix: review 작성 api path 수정

--- frontend/src/api/postReview.ts
+++ frontend/src/api/postReview.ts
@@ -1,11 +1,9 @@
-import { PATH } from '@constants';
-
 import { EmptyObject, ReviewQueryData } from '@custom-types';
 
 import axiosInstance from '@api/axiosInstance';
 
 export const postReview = async ({ studyId, content }: ReviewQueryData) => {
-  const response = await axiosInstance.post<EmptyObject, any, { content: string }>(PATH.REVIEW(studyId), {
+  const response = await axiosInstance.post<EmptyObject, any, { content: string }>(`/api/studies/${studyId}/reviews`, {
     content,
   });
   return response.data;
