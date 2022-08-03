fd598ce3fa0fe3789cca1de4dfab780b53851c43
airman5573 <rpf5573@kakao.com>
2022-08-03 17:13:11 +0900

    feat: local/dev/prod 서버로 분리
    Co-authored-by: TaeYoon <uni613@naver.com>

--- frontend/webpack/webpack.local.js
+++ frontend/webpack/webpack.local.js
@@ -3,12 +3,24 @@ const webpack = require('webpack');
 const { join } = require('path');
 const { merge } = require('webpack-merge');
 
-require('dotenv').config({ path: join(__dirname, '../env/.env.dev') });
+require('dotenv').config({ path: join(__dirname, '../env/.env.local') });
 
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: 'development',
+  devServer: {
+    open: true,
+    port: 3000,
+    compress: true,
+    client: {
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    historyApiFallback: true,
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.API_URL': JSON.stringify(process.env.API_URL),
