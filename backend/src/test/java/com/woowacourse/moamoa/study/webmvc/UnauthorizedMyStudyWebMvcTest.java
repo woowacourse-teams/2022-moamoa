f9e5a044013009471633d3281da32d658d6b49cc
jaejae-yoo <wotj102@gmail.com>
2022-08-04 17:16:57 +0900

    refactor: rest docs 변경

--- backend/src/test/java/com/woowacourse/moamoa/study/webmvc/UnauthorizedMyStudyWebMvcTest.java
+++ backend/src/test/java/com/woowacourse/moamoa/study/webmvc/UnauthorizedMyStudyWebMvcTest.java
@@ -5,7 +5,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
