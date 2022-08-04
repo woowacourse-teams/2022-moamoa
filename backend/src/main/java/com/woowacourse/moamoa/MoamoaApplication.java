88aa874b8939212c90244a65a4a201ac0b0ff238
Donggyu <a29661498@gmail.com>
2022-08-04 17:24:28 +0900

    fix: 불필요한 Bean 제거

--- backend/src/main/java/com/woowacourse/moamoa/MoamoaApplication.java
+++ backend/src/main/java/com/woowacourse/moamoa/MoamoaApplication.java
@@ -1,10 +1,8 @@
 package com.woowacourse.moamoa;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -14,12 +12,5 @@ public class MoamoaApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(MoamoaApplication.class, args);
-    }
-
-    @Bean
-    public ApplicationRunner runner() {
-        return args -> {
-            log.debug("hello !");
-        };
     }
 }
