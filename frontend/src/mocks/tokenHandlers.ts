43e8812b6e79374788b22397a0684d2ace3505fb
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-10 17:46:51 +0900

    feat: tokenHandlers 수정
    refreshToken 로직 추가

--- frontend/src/mocks/tokenHandlers.ts
+++ frontend/src/mocks/tokenHandlers.ts
@@ -1,17 +1,23 @@
 import { rest } from 'msw';
 
 export const tokenHandlers = [
-  rest.post('/api/login/token', (req, res, ctx) => {
+  rest.post('/api/auth/login/token', (req, res, ctx) => {
     const code = req.url.searchParams.get('code');
 
     if (!code) {
       return res(ctx.status(400), ctx.json({ message: '잘못된 요청입니다.' }));
     }
-    return res(ctx.status(200), ctx.json({ accessToken: 'asddfasdfassdf' }));
+    return res(
+      ctx.status(200),
+      ctx.json({ accessToken: 'asddfasdfassdf' }),
+      ctx.cookie('refreshToken', 'test-refreshToken!!!sdfsdf', {
+        // httpOnly: true,
+        // secure: true,
+      }),
+    );
   }),
   rest.get('/api/auth/refresh', (req, res, ctx) => {
-    if (req.cookies.refreshToken === 'test-refreshToken!!!sdfsdf')
-      return res(ctx.status(401), ctx.json({ message: '리프레시 토큰 만료' }), ctx.cookie('refreshToken', ''));
+    if (!req.cookies.refreshToken) return res(ctx.status(401), ctx.json({ message: '리프레시 토큰 없음' }));
 
     return res(
       ctx.status(200),
@@ -21,5 +27,13 @@ export const tokenHandlers = [
         // secure: true,
       }),
     );
+  }),
+  rest.delete('/api/auth/logout', (req, res, ctx) => {
+    const accessTokenString = req.headers.get('Authorization');
+    console.log(accessTokenString);
+    if (!accessTokenString) {
+      return res(ctx.status(400), ctx.json({ message: '토큰 없음' }));
+    }
+    return res(ctx.status(200), ctx.cookie('refreshToken', ''));
   }),
 ];
