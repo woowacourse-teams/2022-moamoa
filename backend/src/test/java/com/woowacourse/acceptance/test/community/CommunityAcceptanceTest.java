df75d2cd95ccac2db1168ceb4ae9663ecfb209e9
jaejae-yoo <wotj102@gmail.com>
2022-08-11 19:36:25 +0900

    test: 스터디 커뮤니티 게시글 수정 인수 테스트

--- backend/src/test/java/com/woowacourse/acceptance/test/community/CommunityAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/acceptance/test/community/CommunityAcceptanceTest.java
@@ -22,6 +22,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.woowacourse.acceptance.AcceptanceTest;
 import com.woowacourse.moamoa.community.service.request.ArticleRequest;
 import com.woowacourse.moamoa.community.service.response.ArticleResponse;
@@ -274,5 +275,59 @@ public class CommunityAcceptanceTest extends AcceptanceTest {
         );
 
         assertThat(response).isEqualTo(new ArticleSummariesResponse(articles, 0, 0, 4));
+    }
+
+    @DisplayName("커뮤니티 글을 수정한다.")
+    @Test
+    void updateArticleToCommunity() throws JsonProcessingException {
+        // arrange
+        long 스터디_ID = 그린론이().로그인하고().자바_스터디를().시작일자는(LocalDate.now()).생성한다();
+        long 게시글_ID = 그린론이().로그인하고().스터디에(스터디_ID).게시글을_작성한다("게시글 제목", "게시글 내용");
+        String 토큰 = 그린론이().로그인한다();
+
+        final ArticleRequest request = new ArticleRequest("게시글 제목 수정", "게시글 내용 수정");
+
+        // act
+        RestAssured.given(spec).log().all()
+                .header(HttpHeaders.AUTHORIZATION, 토큰)
+                .pathParam("study-id", 스터디_ID)
+                .pathParam("article-id", 게시글_ID)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(objectMapper.writeValueAsString(request))
+                .filter(document("update/article",
+                        requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).description("JWT 토큰")
+                        ),
+                        pathParameters(
+                                parameterWithName("study-id").description("스터디 식별 번호"),
+                                parameterWithName("article-id").description("게시글 식별 번호")
+                        ),
+                        requestFields(
+                                fieldWithPath("title").type(JsonFieldType.STRING).description("게시글 수정 제목"),
+                                fieldWithPath("content").type(JsonFieldType.STRING).description("게시글 내용 수정")
+                        )
+                ))
+                .when().log().all()
+                .put("/api/studies/{study-id}/community/articles/{article-id}")
+                .then().log().all()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        // assert
+        final ArticleResponse response = RestAssured
+                .given().log().all()
+                .header(HttpHeaders.AUTHORIZATION, 토큰)
+                .pathParam("study-id", 스터디_ID)
+                .pathParam("article-id", 게시글_ID)
+                .when().log().all()
+                .get("/api/studies/{study-id}/community/articles/{article-id}")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(ArticleResponse.class);
+
+        final AuthorResponse authorResponse = new AuthorResponse(그린론_깃허브_ID, 그린론_이름, 그린론_이미지_URL, 그린론_프로필_URL);
+
+        assertThat(response).isEqualTo(new ArticleResponse(스터디_ID, authorResponse, "게시글 제목",
+                "게시글 내용", LocalDate.now(), LocalDate.now()));
     }
 }
