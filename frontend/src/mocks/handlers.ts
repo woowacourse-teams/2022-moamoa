6713bf90aee813099ba753923152f9094f5b52a1
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-11 13:15:04 +0900

    refactor: 사용하지 않는 로직 제거

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -9,38 +9,6 @@ import { tagHandlers } from '@mocks/tagHandlers';
 import { tokenHandlers } from '@mocks/tokenHandlers';
 
 export const handlers = [
-  rest.get('/api/studies', (req, res, ctx) => {
-    const page = req.url.searchParams.get('page');
-    const size = req.url.searchParams.get('size');
-
-    if ((size === null && page !== null) || (size !== null && page === null)) {
-      return res(ctx.status(400), ctx.json({ message: 'size혹은 page가 없습니다' }));
-    }
-
-    if (page === null && size === null) {
-      return res(
-        ctx.status(200),
-        ctx.json({
-          studies: studyJSON.studies.slice(0, 5),
-          hasNext: true, // TODO: hasNext 조건 주기
-        }),
-      );
-    }
-
-    const sizeNum = Number(size);
-    const pageNum = Number(page);
-    const startIndex = pageNum * sizeNum;
-    const endIndexExclusive = startIndex + sizeNum;
-
-    return res(
-      ctx.status(200),
-      ctx.json({
-        studies: studyJSON.studies.slice(startIndex, endIndexExclusive),
-        hasNext: endIndexExclusive < studyJSON.studies.length,
-      }),
-    );
-  }),
-
   rest.get('/api/studies/search', (req, res, ctx) => {
     const title = req.url.searchParams.get('title');
     const page = req.url.searchParams.get('page');
