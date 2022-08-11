d02748988e43c5d04e6f96c0e876abbb00875e85
Donggyu <a29661498@gmail.com>
2022-08-11 14:13:36 +0900

    fix: 깨지는 테스트 수정

--- backend/src/test/java/com/woowacourse/moamoa/study/controller/MyStudyControllerTest.java
+++ backend/src/test/java/com/woowacourse/moamoa/study/controller/MyStudyControllerTest.java
@@ -51,8 +51,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 import com.woowacourse.moamoa.common.RepositoryTest;
+import com.woowacourse.moamoa.member.domain.Member;
 import com.woowacourse.moamoa.member.domain.repository.MemberRepository;
 import com.woowacourse.moamoa.member.query.data.MemberData;
+import com.woowacourse.moamoa.study.domain.Study;
 import com.woowacourse.moamoa.study.domain.repository.StudyRepository;
 import com.woowacourse.moamoa.study.query.MyStudyDao;
 import com.woowacourse.moamoa.study.service.MyStudyService;
@@ -60,6 +62,7 @@ import com.woowacourse.moamoa.study.service.response.MyStudiesResponse;
 import com.woowacourse.moamoa.study.service.response.MyStudyResponse;
 import com.woowacourse.moamoa.tag.query.response.TagSummaryData;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -85,24 +88,35 @@ class MyStudyControllerTest {
 
     private MyStudyController myStudyController;
 
+    private Member 짱구;
+    private Member 그린론;
+    private Member 디우;
+    private Member 베루스;
+
+    private Study 자바_스터디;
+    private Study 리액트_스터디;
+    private Study 자바스크립트_스터디;
+    private Study HTTP_스터디;
+    private Study 알고리즘_스터디;
+    private Study 리눅스_스터디;
+
     @BeforeEach
     void setUp() {
         myStudyController = new MyStudyController(new MyStudyService(myStudyDao, memberRepository, studyRepository));
 
-        memberRepository.save(짱구);
-        memberRepository.save(그린론);
-        memberRepository.save(디우);
-        memberRepository.save(베루스);
+        짱구 = memberRepository.save(짱구());
+        그린론 = memberRepository.save(그린론());
+        디우 = memberRepository.save(디우());
+        베루스 = memberRepository.save(베루스());
 
-        studyRepository.save(자바_스터디);
-        studyRepository.save(리액트_스터디);
-        studyRepository.save(자바스크립트_스터디);
-        studyRepository.save(HTTP_스터디);
-        studyRepository.save(알고리즘_스터디);
-        studyRepository.save(리눅스_스터디);
+        자바_스터디 = studyRepository.save(자바_스터디(짱구.getId(), Set.of(그린론.getId(), 디우.getId())));
+        리액트_스터디 = studyRepository.save(리액트_스터디(디우.getId(), Set.of(짱구.getId(), 그린론.getId(), 베루스.getId())));
+        자바스크립트_스터디 = studyRepository.save(자바스크립트_스터디(그린론.getId(), Set.of(디우.getId(), 베루스.getId())));
+        HTTP_스터디 = studyRepository.save(HTTP_스터디(디우.getId(), Set.of(베루스.getId(), 짱구.getId())));
+        알고리즘_스터디 = studyRepository.save(알고리즘_스터디(베루스.getId(), Set.of(그린론.getId(), 디우.getId())));
+        리눅스_스터디 = studyRepository.save(리눅스_스터디(베루스.getId(), Set.of(그린론.getId(), 디우.getId())));
 
         entityManager.flush();
-        entityManager.clear();
     }
 
     @DisplayName("내가 참여한 스터디를 조회한다.")
