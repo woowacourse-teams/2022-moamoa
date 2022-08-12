bf7ea037cca199543e46359a29873059883644c2
Donggyu <a29661498@gmail.com>
2022-08-12 10:04:48 +0900

    refactor: 테스트 코드 수정

--- backend/src/test/java/com/woowacourse/moamoa/auth/service/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/moamoa/auth/service/AuthServiceTest.java
@@ -10,7 +10,11 @@ import com.woowacourse.moamoa.auth.infrastructure.TokenProvider;
 import com.woowacourse.moamoa.auth.service.oauthclient.OAuthClient;
 import com.woowacourse.moamoa.auth.service.oauthclient.response.GithubProfileResponse;
 import com.woowacourse.moamoa.auth.service.response.TokenResponse;
+import com.woowacourse.moamoa.auth.service.response.TokenResponseWithRefresh;
+import com.woowacourse.moamoa.common.RepositoryTest;
 import com.woowacourse.moamoa.common.exception.UnauthorizedException;
+import com.woowacourse.moamoa.member.domain.repository.MemberRepository;
+import com.woowacourse.moamoa.member.query.MemberDao;
 import com.woowacourse.moamoa.member.service.MemberService;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,53 +22,55 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@RepositoryTest
 class AuthServiceTest {
 
     private AuthService authService;
 
     private OAuthClient oAuthClient;
 
-    @Autowired
-    private MemberService memberService;
-
-    @Autowired
     private TokenProvider tokenProvider;
+
+    private MemberService memberService;
 
     @Autowired
     private TokenRepository tokenRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private MemberDao memberDao;
+
     @BeforeEach
     void setUp() {
+        memberService = new MemberService(memberRepository, memberDao);
+        tokenProvider = Mockito.mock(TokenProvider.class);
         oAuthClient = Mockito.mock(OAuthClient.class);
         authService = new AuthService(memberService, tokenProvider, oAuthClient, tokenRepository);
+
+        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
+        Mockito.when(oAuthClient.getProfile("access-token"))
+                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
+        Mockito.when(tokenProvider.createToken(1L))
+                .thenReturn(new TokenResponseWithRefresh("accessToken", "refreshToken"));
+        Mockito.when(tokenProvider.recreationAccessToken(1L, "refreshToken"))
+                .thenReturn("recreationAccessToken");
     }
 
     @DisplayName("RefreshToken 을 저장한다.")
     @Test
     public void saveRefreshToken() {
-        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
-        Mockito.when(oAuthClient.getProfile("access-token"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-
         authService.createToken("authorization-code");
         final Token token = tokenRepository.findByGithubId(1L).get();
 
-        assertThat(token.getRefreshToken()).isNotNull();
+        assertThat(token.getRefreshToken()).isEqualTo("refreshToken");
     }
 
-    @DisplayName("RefreshToken 을 이용하여 AccessToken을 업데이트한다.")
+    @DisplayName("RefreshToken 을 이용하여 AccessToken 을 업데이트한다.")
     @Test
     public void updateRefreshToken() {
-        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
-        Mockito.when(oAuthClient.getAccessToken("authorizationCode")).thenReturn("accessToken");
-        Mockito.when(oAuthClient.getProfile("access-token"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-        Mockito.when(oAuthClient.getProfile("accessToken"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-
         authService.createToken("authorization-code");
         final Token token = tokenRepository.findByGithubId(1L).get();
         final String refreshToken = token.getRefreshToken();
@@ -72,16 +78,12 @@ class AuthServiceTest {
         final TokenResponse tokenResponse = authService.refreshToken(1L, refreshToken);
 
         assertThat(refreshToken).isNotBlank();
-        assertThat(tokenResponse.getAccessToken()).isNotBlank();
+        assertThat(tokenResponse.getAccessToken()).isEqualTo("recreationAccessToken");
     }
 
     @DisplayName("DB에 저장되어 있지 않은 refresh token으로 access token을 발급받을 수 없다.")
     @Test
     public void validateRefreshToken() {
-        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
-        Mockito.when(oAuthClient.getProfile("access-token"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-
         assertThatThrownBy(() -> authService.refreshToken(1L, "InvalidRefreshToken"))
                 .isInstanceOf(UnauthorizedException.class);
     }
@@ -89,10 +91,6 @@ class AuthServiceTest {
     @DisplayName("refresh token을 통해 access token을 발급받을 수 있다.")
     @Test
     public void recreationAccessToken() {
-        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
-        Mockito.when(oAuthClient.getProfile("access-token"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-
         authService.createToken("authorization-code");
         final Token token = tokenRepository.findByGithubId(1L).get();
 
@@ -102,10 +100,6 @@ class AuthServiceTest {
     @DisplayName("로그아웃을 하면 Token 을 제거한다.")
     @Test
     public void logout() {
-        Mockito.when(oAuthClient.getAccessToken("authorization-code")).thenReturn("access-token");
-        Mockito.when(oAuthClient.getProfile("access-token"))
-                .thenReturn(new GithubProfileResponse(1L, "dwoo", "imageUrl", "profileUrl"));
-
         authService.createToken("authorization-code");
         final Token token = tokenRepository.findByGithubId(1L).get();
 
