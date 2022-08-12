d8785a2af8f7876f1e5bb891c7a746556707ccc4
Donggyu <a29661498@gmail.com>
2022-08-12 13:42:08 +0900

    refactor: 메소드 분리를 통해 가독성 증가

--- backend/src/main/java/com/woowacourse/moamoa/member/query/MemberDao.java
+++ backend/src/main/java/com/woowacourse/moamoa/member/query/MemberDao.java
@@ -27,17 +27,28 @@ public class MemberDao {
     public List<MemberFullData> findMembersByStudyId(final Long studyId) {
         final String sql = "SELECT member.id, github_id, username, image_url, profile_url, "
                 + "study_member.participation_date as participation_date, "
-                + "((SELECT count(case when (study_member.member_id = member.id) then 1 end) "
-                + "FROM study JOIN study_member ON study.id = study_member.study_id "
-                + "WHERE study_member.member_id = member.id) + "
-                + "(SELECT count(case when (study.owner_id = member.id) then 1 end) "
-                + "FROM study "
-                + "WHERE study.owner_id = member.id)) as number_of_study "
+                + countStudyNumber()
                 + "FROM member JOIN study_member ON member.id = study_member.member_id "
                 + "JOIN study ON study_member.study_id = study.id "
                 + "WHERE study_member.study_id = :id";
 
         return jdbcTemplate.query(sql, Map.of("id", studyId), MEMBER_FULL_DATA_ROW_MAPPER);
+    }
+
+    private String countStudyNumber() {
+        return countOwnerStudy() + " + " + countParticipationStudy();
+    }
+
+    private String countOwnerStudy() {
+        return "((SELECT count(case when (study_member.member_id = member.id) then 1 end) "
+                + "FROM study JOIN study_member ON study.id = study_member.study_id "
+                + "WHERE study_member.member_id = member.id)";
+    }
+
+    private String countParticipationStudy() {
+        return "(SELECT count(case when (study.owner_id = member.id) then 1 end) "
+                + "FROM study "
+                + "WHERE study.owner_id = member.id)) as number_of_study ";
     }
 
     public Optional<MemberData> findByGithubId(final Long githubId) {
