2c778b2de1952c02902e012caed474b7e2e016af
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-08-12 16:17:12 +0900

    feat: UserDescription 컴포넌트 구현

---
+++ frontend/src/pages/study-room-page/components/link-room-tab-panel/components/user-description/UserDescription.style.tsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+
+  padding: 4px 0;
+`;
+
+export const Description = styled.p`
+  margin-left: 8px;
+
+  font-size: 12px;
+  word-break: keep-all;
+`;
