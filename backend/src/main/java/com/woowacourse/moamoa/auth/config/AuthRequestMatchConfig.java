217e0cc07db85f512980f05e460cd290bff7d855
jaejae-yoo <wotj102@gmail.com>
2022-08-12 14:45:32 +0900

    style: 중복된 코드 제거

--- backend/src/main/java/com/woowacourse/moamoa/auth/config/AuthRequestMatchConfig.java
+++ backend/src/main/java/com/woowacourse/moamoa/auth/config/AuthRequestMatchConfig.java
@@ -1,10 +1,5 @@
 package com.woowacourse.moamoa.auth.config;
 
-import static org.springframework.http.HttpMethod.DELETE;
-import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
-import static org.springframework.http.HttpMethod.PUT;
-
 import com.woowacourse.moamoa.auth.controller.matcher.AuthenticationRequestMatcher;
 import com.woowacourse.moamoa.auth.controller.matcher.AuthenticationRequestMatcherBuilder;
 import lombok.AllArgsConstructor;
@@ -15,19 +10,6 @@ import org.springframework.http.HttpMethod;
 @Configuration
 @AllArgsConstructor
 public class AuthRequestMatchConfig {
-
-    @Bean
-    public AuthenticationRequestMatcher authenticationRequestMatcher1() {
-        return new AuthenticationRequestMatcherBuilder()
-                .addUpAuthenticationPath(HttpMethod.POST, "/api/studies", "/api/studies/\\d+/reviews",
-                        "/api/studies/\\d+/reviews/\\d+", "/api/studies/\\w+/community/articles")
-                .addUpAuthenticationPath(HttpMethod.GET, "/api/my/studies", "/api/members/me", "/api/members/me/role",
-                        "/api/studies/\\w+/community/articles/\\w+", "/api/studies/\\w+/community/articles")
-                .addUpAuthenticationPath(HttpMethod.PUT, "/api/studies/\\d+/reviews/\\d+")
-                .addUpAuthenticationPath(HttpMethod.DELETE, "/api/studies/\\d+/reviews/\\d+",
-                        "/api/studies/\\w+/community/articles/\\w+")
-                .build();
-    }
 
     @Bean
     public AuthenticationRequestMatcher authenticationRequestMatcher() {
