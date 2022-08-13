2224c7ae8f9b851212602c50583d37983af8463b
airman5573 <rpf5573@kakao.com>
2022-08-13 15:10:32 +0900

    feat: 게시판 tab panel 기본 레이아웃 생성

---
+++ frontend/src/pages/study-room-page/components/community-tab-panel/CommunityTabPanel.style.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const CommunityTabPanel = styled.div``;
+
+export const Board = styled.div`
+  & > ul {
+  }
+`;
+
+export const ListItem = styled.li`
+  display: flex;
+`;
+
+export const Main = styled.div`
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  color: #1382ce;
+`;
+
+export const Content = styled.div``;
+
+export const MoreInfo = styled.div`
+  display: flex;
+
+  text-align: center;
+  font-size: 13px;
+  line-height: 20px;
+`;
+
+export const Author = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Username = styled.div``;
+
+export const Date = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
