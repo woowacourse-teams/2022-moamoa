e3b0313db2bdffef7f771a979ccafe03c1eea3bc
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-09-12 00:40:15 +0900

    feat: 에러 페이지 컴포넌트 수정

--- frontend/src/pages/error-page/ErrorPage.tsx
+++ frontend/src/pages/error-page/ErrorPage.tsx
@@ -1,19 +1,22 @@
 import sthWentWrongImage from '@assets/images/sth-went-wrong.png';
 
-import * as S from '@error-page/ErrorPage.style';
+import { BoxButton } from '@design/components/button';
+import Flex from '@design/components/flex/Flex';
+import Image from '@design/components/image/Image';
+
 import useErrorPage from '@error-page/hooks/useErrorPage';
 
 const ErrorPage: React.FC = () => {
   const { handleHomeButtonClick } = useErrorPage();
 
   return (
-    <S.Page>
-      <img src={sthWentWrongImage} alt="잘못된 페이지" />
+    <Flex direction="column" justifyContent="center" alignItems="center" rowGap="4px">
+      <Image src={sthWentWrongImage} alt="잘못된 페이지" shape="rectangular" width="auto" height="auto" />
       <p>잘못된 접근입니다.</p>
-      <S.HomeButton type="button" onClick={handleHomeButtonClick}>
+      <BoxButton type="button" onClick={handleHomeButtonClick} fluid={false}>
         홈으로 이동
-      </S.HomeButton>
-    </S.Page>
+      </BoxButton>
+    </Flex>
   );
 };
 
