98921aa8fd1cbce2d6f8219cc2666d73d6ab6f09
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-09-15 16:02:37 +0900

    refactor: StudyChip 수정

--- frontend/src/components/study-chip/StudyChip.tsx
+++ frontend/src/components/study-chip/StudyChip.tsx
@@ -5,7 +5,7 @@ export type StudyChipProps = {
 };
 
 const StudyChip = ({ isOpen }: StudyChipProps) => {
-  return <Chip variant={isOpen ? 'primary' : 'secondary'}>{isOpen ? '모집중' : '모집완료'}</Chip>;
+  return isOpen ? <Chip variant="primary">모집중</Chip> : <Chip variant="secondary">모집완료</Chip>;
 };
 
 export default StudyChip;
