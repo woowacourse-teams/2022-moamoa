e8b713a3ad069cabc1032046a7c8fc80588cef94
airman5573 <rpf5573@kakao.com>
2022-09-19 15:57:24 +0900

    feat: UnstyledButton을 컴포넌트로 분리

--- frontend/src/components/button/unstyled-button/UnstyledButton.tsx
+++ frontend/src/components/button/unstyled-button/UnstyledButton.tsx
@@ -1,3 +1,11 @@
 import * as S from '@components/button/unstyled-button/UnstyledButton.style';
 
-export default S.UnstyledButton;
+type UnstyledButtonProps = {
+  children?: React.ReactNode;
+};
+
+const UnstyledButton: React.FC<UnstyledButtonProps> = ({ children }) => {
+  return <S.UnstyledButton>{children}</S.UnstyledButton>;
+};
+
+export default UnstyledButton;
