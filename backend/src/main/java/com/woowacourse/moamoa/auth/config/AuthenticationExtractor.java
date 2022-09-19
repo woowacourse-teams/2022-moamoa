9a05ad5e6875504caec26d3a7b8492e3a65bc3ac
Donggyu <a29661498@gmail.com>
2022-09-19 16:19:31 +0900

    refactor: 피드백 반영

--- backend/src/main/java/com/woowacourse/moamoa/auth/config/AuthenticationExtractor.java
+++ backend/src/main/java/com/woowacourse/moamoa/auth/config/AuthenticationExtractor.java
@@ -3,6 +3,7 @@ package com.woowacourse.moamoa.auth.config;
 import static org.springframework.http.HttpHeaders.COOKIE;
 import static lombok.AccessLevel.PRIVATE;
 
+import com.woowacourse.moamoa.auth.exception.TokenNotFoundException;
 import java.util.Arrays;
 import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +13,12 @@ import lombok.NoArgsConstructor;
 public class AuthenticationExtractor {
 
     private static final String ACCESS_TOKEN_TYPE = AuthenticationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
+    private static final String COOKIE_DELIMITER = " ";
+    private static final int COOKIE_LENGTH = 2;
+    private static final String ACCESS_TOKEN = "accessToken";
+    private static final String KEY_AND_VALUE_DELIMITER = "=";
+    private static final int KEY_INDEX = 0;
+    private static final int VALUE_INDEX = 1;
 
     public static String extract(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(COOKIE);
@@ -19,19 +26,22 @@ public class AuthenticationExtractor {
         while (headers.hasMoreElements()) {
             String value = headers.nextElement();
 
-            final String accessToken = Arrays.stream(value.split(" "))
-                    .filter(str -> str.startsWith("accessToken"))
-                    .findAny().orElseThrow();
+            final String accessToken = Arrays.stream(value.split(COOKIE_DELIMITER))
+                    .filter(str -> str.startsWith(ACCESS_TOKEN))
+                    .findAny()
+                    .orElseThrow(TokenNotFoundException::new);
 
-            final String[] splitAccessToken = accessToken.split("=");
+            final String[] splitAccessToken = accessToken.split(KEY_AND_VALUE_DELIMITER);
 
-            if (splitAccessToken.length < 2) {
+            if (splitAccessToken.length < COOKIE_LENGTH) {
                 return null;
             }
 
-            if (splitAccessToken[0].equals("accessToken")) {
-                String authHeaderValue = splitAccessToken[1];
-                request.setAttribute(ACCESS_TOKEN_TYPE, splitAccessToken[1]);
+            final String cookieKey = splitAccessToken[KEY_INDEX];
+
+            if (cookieKey.equals(ACCESS_TOKEN)) {
+                String authHeaderValue = splitAccessToken[VALUE_INDEX];
+                request.setAttribute(ACCESS_TOKEN_TYPE, splitAccessToken[VALUE_INDEX]);
                 int commaIndex = authHeaderValue.indexOf(',');
 
                 if (commaIndex > 0) {
