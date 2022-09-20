f4fd8c675a84c3c0ca2be8bb557b944a2386213f
airman5573 <rpf5573@kakao.com>
2022-09-20 15:03:58 +0900

    refactor: css 순서 정리

--- frontend/src/components/multi-tag-select/MultiTagSelect.style.tsx
+++ frontend/src/components/multi-tag-select/MultiTagSelect.style.tsx
@@ -5,81 +5,84 @@ import { UnstyledButton } from '@components/button/unstyled-button/UnstyledButto
 import DropDownBox from '@components/drop-down-box/DropDownBox';
 
 export const Container = styled.div`
-  width: 100%;
   position: relative;
+  width: 100%;
 `;
 
 export const SelectControl = styled.div`
-  -webkit-box-align: center;
+  position: relative;
+
+  display: flex;
+  flex-wrap: wrap;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
   align-items: center;
+
+  min-height: 38px;
+
+  -webkit-box-align: center;
   background-color: rgb(255, 255, 255);
   border-color: rgb(204, 204, 204);
   border-radius: 4px;
   border-style: solid;
   border-width: 1px;
-  display: flex;
-  flex-wrap: wrap;
-  -webkit-box-pack: justify;
-  justify-content: space-between;
-  min-height: 38px;
-  position: relative;
-  box-sizing: border-box;
   padding-right: 8px;
 `;
 
 export const SelectedOptionList = styled.ul`
-  -webkit-box-align: center;
-  align-items: center;
+  position: relative;
+
   display: flex;
   flex: 1 1 0%;
   flex-wrap: wrap;
+  align-items: center;
+
+  -webkit-box-align: center;
   padding: 2px 8px;
-  position: relative;
   overflow: hidden;
-  box-sizing: border-box;
 `;
 
 export const SelectedOptionListItem = styled.li`
   ${({ theme }) => css`
+    display: flex;
+
     background-color: ${theme.colors.secondary.base};
     border-radius: 2px;
-    display: flex;
     margin: 2px;
-    min-width: 0;
-    box-sizing: border-box;
     font-size: 14px;
   `}
 `;
 
 export const SelectedOption = styled.div`
+  padding: 3px 3px 3px 6px;
+  font-size: 12px;
+
   border-radius: 2px;
   color: rgb(51, 51, 51);
-  font-size: 12px;
   overflow: hidden;
-  padding: 3px 3px 3px 6px;
   text-overflow: ellipsis;
   white-space: nowrap;
-  box-sizing: border-box;
 `;
 
 export const RemoveSelectedOptionButton = styled(UnstyledButton)`
-  -webkit-box-align: center;
-  align-items: center;
-  border-radius: 2px;
   display: flex;
+  align-items: center;
+
+  font-size: 14px;
+
+  -webkit-box-align: center;
+  border-radius: 2px;
   padding-left: 4px;
   padding-right: 4px;
-  box-sizing: border-box;
-  font-size: 14px;
 `;
 
 export const Indicators = styled.div`
-  -webkit-box-align: center;
+  display: flex;
   align-items: center;
   align-self: stretch;
-  display: flex;
+  -webkit-box-align: center;
+
   flex-shrink: 0;
-  box-sizing: border-box;
   row-gap: 10px;
 `;
 
