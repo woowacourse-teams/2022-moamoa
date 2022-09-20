e42e214c14a8e53d13a3f9f8e4da247881db22e9
airman5573 <rpf5573@kakao.com>
2022-09-20 20:55:32 +0900

    refactor: 함수의 작동 방식을 명확하게 기술한다

--- frontend/src/utils/isNullOrUndefined.ts
+++ frontend/src/utils/isNullOrUndefined.ts
@@ -1,1 +1,3 @@
-export default (value: unknown): value is null | undefined => value == null;
+const isNullOrUndefined = (value: unknown): value is null | undefined => value === undefined || value === null;
+
+export default isNullOrUndefined;
