50c09453ca98a63597b129e48bdc273217b835a0
airman5573 <rpf5573@kakao.com>
2022-09-23 10:56:54 +0900

    perf: code splitting을 하지 않는다

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,13 @@
-import { Suspense, lazy } from 'react';
+import {
+  CreateStudyPage,
+  DetailPage,
+  EditStudyPage,
+  ErrorPage,
+  LoginRedirectPage,
+  MainPage,
+  MyStudyPage,
+  StudyRoomPage,
+} from '@pages';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 import { PATH } from '@constants';
@@ -7,81 +16,63 @@ import { useAuth } from '@hooks/useAuth';
 
 import { Footer, Header, Main } from '@layout';
 
-import EditStudyPage from '@pages/study-page/edit-study-page/EditStudyPage';
-
-const CreateStudyPage = lazy(
-  () => import(/* webpackChunkName: "create-study-page" */ '@pages/create-study-page/CreateStudyPage'),
-);
-const DetailPage = lazy(() => import(/* webpackChunkName: "detail-page" */ '@pages/detail-page/DetailPage'));
-const ErrorPage = lazy(() => import(/* webpackChunkName: "error-page" */ '@pages/error-page/ErrorPage'));
-const LoginRedirectPage = lazy(
-  () => import(/* webpackChunkName: "login-redirect-page" */ '@pages/login-redirect-page/LoginRedirectPage'),
-);
-const MainPage = lazy(() => import(/* webpackChunkName: "main-page" */ '@pages/main-page/MainPage'));
-const MyStudyPage = lazy(() => import(/* webpackChunkName: "my-study-page" */ '@pages/my-study-page/MyStudyPage'));
-const StudyRoomPage = lazy(
-  () => import(/* webpackChunkName: "study-room-page" */ '@pages/study-room-page/StudyRoomPage'),
-);
-
 const App = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <>
       <Header />
-      <Suspense fallback={<></>}>
-        <Main>
-          <Routes>
-            <Route path={PATH.MAIN} element={<MainPage />} />
-            <Route path={PATH.STUDY_DETAIL()} element={<DetailPage />} />
-            <Route
-              path={PATH.CREATE_STUDY}
-              element={isLoggedIn ? <CreateStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={PATH.LOGIN}
-              element={isLoggedIn ? <Navigate to={PATH.MAIN} replace={true} /> : <LoginRedirectPage />}
-            />
-            <Route
-              path={PATH.MY_STUDY}
-              element={isLoggedIn ? <MyStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.STUDY_ROOM()}/*`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.COMMUNITY_ARTICLE()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.COMMUNITY_PUBLISH()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.COMMUNITY_EDIT()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.NOTICE_ARTICLE()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.NOTICE_PUBLISH()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={`${PATH.NOTICE_EDIT()}`}
-              element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route
-              path={PATH.EDIT_STUDY()}
-              element={isLoggedIn ? <EditStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-            />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </Main>
-      </Suspense>
+      <Main>
+        <Routes>
+          <Route path={PATH.MAIN} element={<MainPage />} />
+          <Route path={PATH.STUDY_DETAIL()} element={<DetailPage />} />
+          <Route
+            path={PATH.CREATE_STUDY}
+            element={isLoggedIn ? <CreateStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={PATH.LOGIN}
+            element={isLoggedIn ? <Navigate to={PATH.MAIN} replace={true} /> : <LoginRedirectPage />}
+          />
+          <Route
+            path={PATH.MY_STUDY}
+            element={isLoggedIn ? <MyStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.STUDY_ROOM()}/*`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.COMMUNITY_ARTICLE()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.COMMUNITY_PUBLISH()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.COMMUNITY_EDIT()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.NOTICE_ARTICLE()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.NOTICE_PUBLISH()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={`${PATH.NOTICE_EDIT()}`}
+            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route
+            path={PATH.EDIT_STUDY()}
+            element={isLoggedIn ? <EditStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
+          />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Main>
       <Footer />
     </>
   );
