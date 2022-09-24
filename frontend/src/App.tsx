68ffbc2a3dfdd5fe881d076b6774f20adb63e5e4
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-09-25 00:33:59 +0900

    refactor: App.tsx 라우팅 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,12 @@
 import NoticeTabPanel from '@notice-tab/NoticeTabPanel';
+import { Route, Routes } from 'react-router-dom';
+
+import { PATH } from '@constants';
+
+import { useAuth } from '@hooks/useAuth';
+
+import { Footer, Header, Main } from '@layout';
+
 import {
   CreateStudyPage,
   DetailPage,
@@ -9,13 +17,8 @@ import {
   MyStudyPage,
   StudyRoomPage,
 } from '@pages';
-import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { PATH } from '@constants';
-
-import { useAuth } from '@hooks/useAuth';
-
-import { Footer, Header, Main } from '@layout';
+import RouteWithCondition from '@components/route-with-condition/RouteWithCondition';
 
 import LinkRoomTabPanel from '@study-room-page/tabs/link-room-tab-panel/LinkRoomTabPanel';
 import ReviewTabPanel from '@study-room-page/tabs/review-tab-panel/ReviewTabPanel';
@@ -32,40 +35,30 @@ const App = () => {
         <Routes>
           <Route path={PATH.MAIN} element={<MainPage />} />
           <Route path={PATH.STUDY_DETAIL()} element={<DetailPage />} />
-          <Route
-            path={PATH.CREATE_STUDY}
-            element={isLoggedIn ? <CreateStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-          />
-          <Route
-            path={PATH.EDIT_STUDY()}
-            element={isLoggedIn ? <EditStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-          />
-          <Route
-            path={PATH.LOGIN}
-            element={isLoggedIn ? <Navigate to={PATH.MAIN} replace={true} /> : <LoginRedirectPage />}
-          />
-          <Route
-            path={PATH.MY_STUDY}
-            element={isLoggedIn ? <MyStudyPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-          />
-          <Route
-            path={PATH.STUDY_ROOM()}
-            element={isLoggedIn ? <StudyRoomPage /> : <Navigate to={PATH.MAIN} replace={true} />}
-          >
-            <Route index element={<NoticeTabPanel />} />
-            <Route path={PATH.NOTICE} element={<NoticeTabPanel />}>
-              <Route path={PATH.NOTICE_PUBLISH} element={<NoticeTabPanel />} />
-              <Route path={PATH.NOTICE_ARTICLE()} element={<NoticeTabPanel />} />
-              <Route path={PATH.NOTICE_EDIT()} element={<NoticeTabPanel />} />
+          <Route element={<RouteWithCondition routingCondition={!isLoggedIn} />}>
+            <Route path={PATH.LOGIN} element={<LoginRedirectPage />} />
+          </Route>
+          <Route element={<RouteWithCondition routingCondition={isLoggedIn} />}>
+            <Route path={PATH.CREATE_STUDY} element={<CreateStudyPage />} />
+            <Route path={PATH.EDIT_STUDY()} element={<EditStudyPage />} />
+            <Route path={PATH.MY_STUDY} element={<MyStudyPage />} />
+            <Route path={PATH.STUDY_ROOM()} element={<StudyRoomPage />}>
+              {/* TODO: 인덱스 페이지를 따로 두면 좋을 것 같다. */}
+              <Route index element={<NoticeTabPanel />} />
+              <Route path={PATH.NOTICE} element={<NoticeTabPanel />}>
+                {[PATH.NOTICE_PUBLISH, PATH.NOTICE_ARTICLE(), PATH.NOTICE_EDIT()].map((path, index) => (
+                  <Route key={index} path={path} element={<NoticeTabPanel />} />
+                ))}
+              </Route>
+              <Route path={PATH.COMMUNITY} element={<CommunityTabPanel />}>
+                {[PATH.COMMUNITY_PUBLISH, PATH.COMMUNITY_ARTICLE(), PATH.COMMUNITY_EDIT()].map((path, index) => (
+                  <Route key={index} path={path} element={<CommunityTabPanel />} />
+                ))}
+              </Route>
+              <Route path={PATH.LINK} element={<LinkRoomTabPanel />} />
+              <Route path={PATH.REVIEW} element={<ReviewTabPanel />} />
+              <Route path="*" element={<ErrorPage />} />
             </Route>
-            <Route path={PATH.COMMUNITY} element={<CommunityTabPanel />}>
-              <Route path={PATH.COMMUNITY_PUBLISH} element={<CommunityTabPanel />} />
-              <Route path={PATH.COMMUNITY_ARTICLE()} element={<CommunityTabPanel />} />
-              <Route path={PATH.COMMUNITY_EDIT()} element={<CommunityTabPanel />} />
-            </Route>
-            <Route path={PATH.LINK} element={<LinkRoomTabPanel />} />
-            <Route path={PATH.REVIEW} element={<ReviewTabPanel />} />
-            <Route path="*" element={<ErrorPage />} />
           </Route>
           <Route path="*" element={<ErrorPage />} />
         </Routes>
