2190e2f48c6420c4a0708750eadee81a90acaa5d
jaejae-yoo <wotj102@gmail.com>
2022-09-29 11:07:47 +0900

    refactor: studyPlanner, recruitPlanner -> updatingStudy, sreatingStudy 클래스로 변경

--- backend/src/test/java/com/woowacourse/moamoa/study/domain/CreatingStudyTest.java
+++ backend/src/test/java/com/woowacourse/moamoa/study/domain/CreatingStudyTest.java
@@ -8,7 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.moamoa.study.domain.exception.InvalidPeriodException;
-import com.woowacourse.moamoa.study.service.exception.InvalidUpdatingException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +15,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-public class RecruitPlannerTest {
+public class CreatingStudyTest {
 
     @DisplayName("RECRUIT Planner| 스터디 상태 생성 | 모집 마감일 < 생성일 예외")
     @Test
@@ -90,10 +89,36 @@ public class RecruitPlannerTest {
         assertThat(study.getRecruitPlanner().getRecruitStatus()).isEqualTo(RECRUITMENT_START);
     }
 
-    @DisplayName("Recruit Planner| 스터디 상태 수정| 모집 마감 날짜 수정일이 생성일 이전으로 수정 OR 수정 인원이 현재 인원 보다 적은 경우 예외")
+    @DisplayName("STUDY Planner| 스터디 생성 | 시작이 종료보다 이후인 경우 OR 생성이 시작보다 이후인 경우 예외")
     @ParameterizedTest
-    @CsvSource({"1,3", "0,2"})
-    void updateRecruitPlannerException(int modifyCreateDate, int memberCount) {
+    @CsvSource({"0,1", "1,0"})
+    void createStudyPlannerStatusException(int modifyStartDate, int modifyEndDate) {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        //target
+        LocalDate startDate = now.toLocalDate().minusDays(modifyStartDate);
+        LocalDate endDate = now.toLocalDate().minusDays(modifyEndDate);
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(1, RECRUITMENT_START, enrollmentEndDate);
+
+        //when && then
+        assertThatThrownBy(
+                () -> {
+                    StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+                    new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+                })
+                .isInstanceOf(InvalidPeriodException.class)
+                .hasMessageContaining("잘못된 기간 설정입니다.");
+    }
+
+    @DisplayName("STUDY Planner| 스터디 생성 |시작 == 생성 IN PROGRESS")
+    @Test
+    void createStudyPlannerStatusIsProgress() {
         //given
         LocalDateTime now = now();
         LocalDateTime createdAt = now;
@@ -104,78 +129,40 @@ public class RecruitPlannerTest {
 
         Content content = new Content("title", "excerpt", "thumbnail", "description");
         Participants participants = Participants.createBy(1L);
-        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+
+        //target
+        RecruitPlanner recruitPlanner = new RecruitPlanner(2, RECRUITMENT_START, enrollmentEndDate);
         StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
 
         //when
         Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
-        study.participate(2L);
-        study.participate(3L);
 
-        //when && then
-        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(memberCount, RECRUITMENT_END, createdAt.toLocalDate()
-                .minusDays(modifyCreateDate));
-
-        assertThatThrownBy(() -> {
-            study.updatePlanners(updateRecruitPlanner, studyPlanner, now);
-            study.updateContent(1L, content, AttachedTags.empty());
-        }).isInstanceOf(InvalidUpdatingException.class)
-                .hasMessageContaining("스터디 수정이 불가능합니다.");
+        //then
+        assertThat(study.getStudyPlanner().getStudyStatus()).isEqualTo(IN_PROGRESS);
     }
 
-    @DisplayName("Recruit Planner| 스터디 상태 수정| 수정일이 모집 마감 날짜 이전 AND 수정 인원이 현재 인원과 같은 경우 END")
+    @DisplayName("STUDY Planner| 스터디 생성 | 예외와 IN PROGRESS 이외의 경우 PREPARE")
     @Test
-    void updateRecruitPlannerIsEnd() {
-        //given
-        LocalDateTime now = now();
-        LocalDateTime createdAt = now;
-
-        LocalDate enrollmentEndDate = now.toLocalDate().plusDays(2);
-        LocalDate startDate = now.toLocalDate().plusDays(2);
-        LocalDate endDate = now.toLocalDate().plusDays(3);
-
-        Content content = new Content("title", "excerpt", "thumbnail", "description");
-        Participants participants = Participants.createBy(1L);
-        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
-        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
-
-        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
-        study.participate(2L);
-        study.participate(3L);
-
-        //when && then
-        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
-        study.updatePlanners(updateRecruitPlanner, studyPlanner, now.plusDays(3));
-        study.updateContent(1L, content, AttachedTags.empty());
-
-        assertThat(study.getRecruitPlanner().getRecruitStatus()).isEqualTo(RECRUITMENT_END);
-    }
-
-    @DisplayName("Recruit Planner| 스터디 상태 수정| 수정일이 모집 마감 날짜 이전 AND 수정 인원이 현재 인원과 같은 경우 START")
-    @Test
-    void updateRecruitPlannerIsStart() {
+    void createStudyPlannerStatusIsPrepare() {
         //given
         LocalDateTime now = now();
         LocalDateTime createdAt = now;
 
         LocalDate enrollmentEndDate = now.toLocalDate();
         LocalDate startDate = now.toLocalDate();
-        LocalDate endDate = now.toLocalDate().plusDays(3);
+        LocalDate endDate = now.toLocalDate().plusDays(1);
 
         Content content = new Content("title", "excerpt", "thumbnail", "description");
         Participants participants = Participants.createBy(1L);
-        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+
+        //target
+        RecruitPlanner recruitPlanner = new RecruitPlanner(2, RECRUITMENT_START, enrollmentEndDate);
         StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
 
+        //when
         Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
-        study.participate(2L);
-        study.participate(3L);
 
-        //when && then
-        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(4, RECRUITMENT_END, enrollmentEndDate);
-        study.updatePlanners(updateRecruitPlanner, studyPlanner, now);
-        study.updateContent(1L, content, AttachedTags.empty());
-
-        assertThat(study.getRecruitPlanner().getRecruitStatus()).isEqualTo(RECRUITMENT_START);
+        //then
+        assertThat(study.getStudyPlanner().getStudyStatus()).isEqualTo(IN_PROGRESS);
     }
 }
---
+++ backend/src/test/java/com/woowacourse/moamoa/study/domain/UpdatingStudyTest.java
@@ -0,0 +1,226 @@
+package com.woowacourse.moamoa.study.domain;
+
+import static com.woowacourse.moamoa.study.domain.RecruitStatus.RECRUITMENT_END;
+import static com.woowacourse.moamoa.study.domain.RecruitStatus.RECRUITMENT_START;
+import static com.woowacourse.moamoa.study.domain.StudyStatus.DONE;
+import static com.woowacourse.moamoa.study.domain.StudyStatus.IN_PROGRESS;
+import static com.woowacourse.moamoa.study.domain.StudyStatus.PREPARE;
+import static java.time.LocalDateTime.now;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.woowacourse.moamoa.study.service.exception.InvalidUpdatingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class UpdatingStudyTest {
+
+    @DisplayName("Recruit Planner| 스터디 상태 수정| 모집 마감 날짜 수정일이 생성일 이전으로 수정 OR 수정 인원이 현재 인원 보다 적은 경우 예외")
+    @ParameterizedTest
+    @CsvSource({"1,3", "0,2"})
+    void updateRecruitPlannerException(int modifyCreateDate, int memberCount) {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate();
+        LocalDate endDate = now.toLocalDate().plusDays(1);
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        //when
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when && then
+        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(memberCount, RECRUITMENT_END, createdAt.toLocalDate()
+                .minusDays(modifyCreateDate));
+
+        assertThatThrownBy(() -> {
+            study.updatePlanners(updateRecruitPlanner, studyPlanner, now);
+            study.updateContent(1L, content, AttachedTags.empty());
+        }).isInstanceOf(InvalidUpdatingException.class)
+                .hasMessageContaining("스터디 수정이 불가능합니다.");
+    }
+
+    @DisplayName("Recruit Planner| 스터디 상태 수정| 수정일이 모집 마감 날짜 이전 AND 수정 인원이 현재 인원과 같은 경우 END")
+    @Test
+    void updateRecruitPlannerIsEnd() {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate().plusDays(2);
+        LocalDate startDate = now.toLocalDate().plusDays(2);
+        LocalDate endDate = now.toLocalDate().plusDays(3);
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when && then
+        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+        study.updatePlanners(updateRecruitPlanner, studyPlanner, now.plusDays(3));
+        study.updateContent(1L, content, AttachedTags.empty());
+
+        assertThat(study.getRecruitPlanner().getRecruitStatus()).isEqualTo(RECRUITMENT_END);
+    }
+
+    @DisplayName("Recruit Planner| 스터디 상태 수정| 수정일이 모집 마감 날짜 이전 AND 수정 인원이 현재 인원과 같은 경우 START")
+    @Test
+    void updateRecruitPlannerIsStart() {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate();
+        LocalDate endDate = now.toLocalDate().plusDays(3);
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when && then
+        final RecruitPlanner updateRecruitPlanner = new RecruitPlanner(4, RECRUITMENT_END, enrollmentEndDate);
+        study.updatePlanners(updateRecruitPlanner, studyPlanner, now);
+        study.updateContent(1L, content, AttachedTags.empty());
+
+        assertThat(study.getRecruitPlanner().getRecruitStatus()).isEqualTo(RECRUITMENT_START);
+    }
+
+    @DisplayName("Study Planner| 스터디 수정| 스터디 시작이 종료 이후 OR 스터디 생성이 시작 이후 예외")
+    @ParameterizedTest
+    @CsvSource({"1, 0", "-1, 0"})
+    void updateStudyPlannerException(int startDay, int endDay) {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate();
+        LocalDate endDate = now.toLocalDate();
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(3, RECRUITMENT_END, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when && then
+        assertThatThrownBy(() -> {
+            StudyPlanner updateStudyPlanner = new StudyPlanner(startDate.plusDays(startDay), endDate.plusDays(endDay), IN_PROGRESS);
+            study.updatePlanners(recruitPlanner, updateStudyPlanner, now);
+            study.updateContent(1L, content, AttachedTags.empty());
+        });
+    }
+
+    @DisplayName("Study Planner| 스터디 수정| 수정하는 날이 종료일 이후인 경우 DONE")
+    @Test
+    void updateStudyPlannerIsDone() {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate();
+        LocalDate endDate = now.toLocalDate();
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(4, RECRUITMENT_END, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when
+        final StudyPlanner updateStudyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+        study.updatePlanners(recruitPlanner, updateStudyPlanner, now.plusDays(1));
+        study.updateContent(1L, content, AttachedTags.empty());
+
+        //then
+        assertThat(study.getStudyPlanner().getStudyStatus()).isEqualTo(DONE);
+    }
+
+    @DisplayName("Study Planner| 스터디 수정| 수정하는 날이 시작일 이전인 경우 PREPARE")
+    @Test
+    void updateStudyPlannerIsPrepare() {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate().plusDays(3);
+        LocalDate endDate = now.toLocalDate().plusDays(4);
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(4, RECRUITMENT_START, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, PREPARE);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when
+        final StudyPlanner updateStudyPlanner = new StudyPlanner(startDate, endDate, PREPARE);
+        study.updatePlanners(recruitPlanner, updateStudyPlanner, now.plusDays(2));
+        study.updateContent(1L, content, AttachedTags.empty());
+
+        //then
+        assertThat(study.getStudyPlanner().getStudyStatus()).isEqualTo(PREPARE);
+    }
+
+    @DisplayName("Study Planner| 스터디 수정| 수정하는 날이 종료일 이후거나 수정하는 날이 시작일 이전인 경우가 아니면 IN_PROGRESS")
+    @Test
+    void updateStudyPlannerIsInProgress() {
+        //given
+        LocalDateTime now = now();
+        LocalDateTime createdAt = now;
+
+        LocalDate enrollmentEndDate = now.toLocalDate();
+        LocalDate startDate = now.toLocalDate();
+        LocalDate endDate = now.toLocalDate();
+
+        Content content = new Content("title", "excerpt", "thumbnail", "description");
+        Participants participants = Participants.createBy(1L);
+        RecruitPlanner recruitPlanner = new RecruitPlanner(4, RECRUITMENT_START, enrollmentEndDate);
+        StudyPlanner studyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+
+        Study study = new Study(content, participants, recruitPlanner, studyPlanner, AttachedTags.empty(), createdAt);
+        study.participate(2L);
+        study.participate(3L);
+
+        //when
+        final StudyPlanner updateStudyPlanner = new StudyPlanner(startDate, endDate, IN_PROGRESS);
+        study.updatePlanners(recruitPlanner, updateStudyPlanner, now);
+        study.updateContent(1L, content, AttachedTags.empty());
+
+        //then
+        assertThat(study.getStudyPlanner().getStudyStatus()).isEqualTo(IN_PROGRESS);
+    }
+}
