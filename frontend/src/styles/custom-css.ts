54ffe4cc2fedb45f31b36822925a2163d2706257
airman5573 <rpf5573@kakao.com>
2022-09-30 17:41:59 +0900

    feat: custom css를 주는 모듈 추가

---
+++ frontend/src/styles/custom-css.ts
@@ -0,0 +1,60 @@
+import { css } from '@emotion/react';
+import { type CSSProperties as OriginalCSSProperties } from '@emotion/serialize';
+
+import { BreakPoint, BreakpointsFor, mqDown } from '@styles/responsive';
+
+type KeyOfOriginalCSSProperties = keyof OriginalCSSProperties;
+
+export type PickAllowedCSSProperties<CSSProperties extends KeyOfOriginalCSSProperties> = Pick<
+  OriginalCSSProperties,
+  CSSProperties
+>;
+
+export type CSSPropertyWithValue<AllowedCSSProperties extends KeyOfOriginalCSSProperties> = {
+  [K in keyof PickAllowedCSSProperties<AllowedCSSProperties>]: OriginalCSSProperties[K];
+};
+
+export type ResponsiveCSSPropertyWithValue<AllowedCSSProperties extends KeyOfOriginalCSSProperties> = BreakpointsFor<
+  CSSPropertyWithValue<AllowedCSSProperties>
+>;
+
+export type CustomCSS<AllowedCSSProperties extends KeyOfOriginalCSSProperties> =
+  CSSPropertyWithValue<AllowedCSSProperties> & ResponsiveCSSPropertyWithValue<AllowedCSSProperties>;
+
+export const getResponsiveStyle = <AllowedCSSProperties extends KeyOfOriginalCSSProperties>(
+  breakPoint: BreakPoint,
+  styleObject: CSSPropertyWithValue<AllowedCSSProperties>,
+) => {
+  return css`
+    ${mqDown(breakPoint)} {
+      ${styleObject}
+    }
+  `;
+};
+
+export const resolveCustomCSS = <AllowedCSSProperties extends KeyOfOriginalCSSProperties>(
+  custom?: CustomCSS<AllowedCSSProperties>,
+) => {
+  if (!custom) return css``;
+  const { xs, sm, md, lg, xl, xxl, xxxl, ...defaultStyle } = custom;
+
+  const xsStyle = xs && getResponsiveStyle<AllowedCSSProperties>('xs', xs);
+  const smStyle = sm && getResponsiveStyle<AllowedCSSProperties>('sm', sm);
+  const mdStyle = md && getResponsiveStyle<AllowedCSSProperties>('md', md);
+  const lgStyle = lg && getResponsiveStyle<AllowedCSSProperties>('lg', lg);
+  const xlStyle = xl && getResponsiveStyle<AllowedCSSProperties>('xl', xl);
+  const xxlStyle = xxl && getResponsiveStyle<AllowedCSSProperties>('xxl', xxl);
+  const xxxlStyle = xxxl && getResponsiveStyle<AllowedCSSProperties>('xxxl', xxxl);
+
+  return css`
+    ${defaultStyle}
+
+    ${xxxlStyle}
+    ${xxlStyle}
+    ${xlStyle}
+    ${lgStyle}
+    ${mdStyle}
+    ${smStyle}
+    ${xsStyle}
+  `;
+};
