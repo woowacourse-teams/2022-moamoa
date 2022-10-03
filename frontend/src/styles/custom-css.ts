7022bdde987e6da6b8c10c1adea965b687c0c227
airman5573 <rpf5573@kakao.com>
2022-10-03 13:54:58 +0900

    feat: custom css를 넣을 수 있는 기능 추가

--- frontend/src/styles/custom-css.ts
+++ frontend/src/styles/custom-css.ts
@@ -19,7 +19,7 @@ export type ResponsiveCSSPropertyWithValue<AllowedCSSProperties extends KeyOfOri
 >;
 
 export type CustomCSS<AllowedCSSProperties extends KeyOfOriginalCSSProperties> =
-  CSSPropertyWithValue<AllowedCSSProperties> & ResponsiveCSSPropertyWithValue<AllowedCSSProperties>;
+  CSSPropertyWithValue<AllowedCSSProperties> & { responsive?: ResponsiveCSSPropertyWithValue<AllowedCSSProperties> };
 
 export const getResponsiveStyle = <AllowedCSSProperties extends KeyOfOriginalCSSProperties>(
   breakPoint: BreakPoint,
@@ -36,25 +36,32 @@ export const resolveCustomCSS = <AllowedCSSProperties extends KeyOfOriginalCSSPr
   custom?: CustomCSS<AllowedCSSProperties>,
 ) => {
   if (!custom) return css``;
-  const { xs, sm, md, lg, xl, xxl, xxxl, ...defaultStyle } = custom;
+  const { responsive, ...defaultStyle } = custom;
 
-  const xsStyle = xs && getResponsiveStyle<AllowedCSSProperties>('xs', xs);
-  const smStyle = sm && getResponsiveStyle<AllowedCSSProperties>('sm', sm);
-  const mdStyle = md && getResponsiveStyle<AllowedCSSProperties>('md', md);
-  const lgStyle = lg && getResponsiveStyle<AllowedCSSProperties>('lg', lg);
-  const xlStyle = xl && getResponsiveStyle<AllowedCSSProperties>('xl', xl);
-  const xxlStyle = xxl && getResponsiveStyle<AllowedCSSProperties>('xxl', xxl);
-  const xxxlStyle = xxxl && getResponsiveStyle<AllowedCSSProperties>('xxxl', xxxl);
+  if (responsive) {
+    const { xs, sm, md, lg, xl, xxl, xxxl } = responsive;
+    const xsStyle = xs && getResponsiveStyle<AllowedCSSProperties>('xs', xs);
+    const smStyle = sm && getResponsiveStyle<AllowedCSSProperties>('sm', sm);
+    const mdStyle = md && getResponsiveStyle<AllowedCSSProperties>('md', md);
+    const lgStyle = lg && getResponsiveStyle<AllowedCSSProperties>('lg', lg);
+    const xlStyle = xl && getResponsiveStyle<AllowedCSSProperties>('xl', xl);
+    const xxlStyle = xxl && getResponsiveStyle<AllowedCSSProperties>('xxl', xxl);
+    const xxxlStyle = xxxl && getResponsiveStyle<AllowedCSSProperties>('xxxl', xxxl);
+
+    return css`
+      ${defaultStyle}
+      // 우선순위가 중요합니다!! xxxl -> xs 순으로 놓아야 합니다
+      ${xxxlStyle}
+      ${xxlStyle}
+      ${xlStyle}
+      ${lgStyle}
+      ${mdStyle}
+      ${smStyle}
+      ${xsStyle}
+    `;
+  }
 
   return css`
     ${defaultStyle}
-
-    ${xxxlStyle}
-    ${xxlStyle}
-    ${xlStyle}
-    ${lgStyle}
-    ${mdStyle}
-    ${smStyle}
-    ${xsStyle}
   `;
 };
