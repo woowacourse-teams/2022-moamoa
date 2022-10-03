4c650e70a5c3b8d401c544301715cfb47e3a1d5a
airman5573 <rpf5573@kakao.com>
2022-10-03 21:44:17 +0900

    feat: StudyMemberSection 개선

--- frontend/src/pages/detail-page/components/study-member-section/StudyMemberSection.tsx
+++ frontend/src/pages/detail-page/components/study-member-section/StudyMemberSection.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { DEFAULT_VISIBLE_STUDY_MEMBER_CARD_COUNT } from '@constants';
@@ -15,7 +16,7 @@ import { theme } from '@styles/theme';
 import { CrownIcon } from '@components/icons';
 import SectionTitle from '@components/section-title/SectionTitle';
 
-import MoreButton from '@detail-page/components/more-button/MoreButton';
+import { default as ImportedMoreButton, MoreButtonProps } from '@detail-page/components/more-button/MoreButton';
 import StudyMemberCard from '@detail-page/components/study-member-card/StudyMemberCard';
 
 export type StudyMemberSectionProps = {
@@ -28,78 +29,12 @@ const StudyMemberSection: React.FC<StudyMemberSectionProps> = ({ owner, members 
 
   const totalMembers = [owner, ...members];
 
+  const hasStudyMembers = !(totalMembers.length === 0);
+
+  const isOverDefaultMemberCount = totalMembers.length > DEFAULT_VISIBLE_STUDY_MEMBER_CARD_COUNT;
+
   const handleShowMoreButtonClick = () => {
     setShowAll(prev => !prev);
-  };
-
-  const renderMembers = () => {
-    if (totalMembers.length === 0) {
-      return <li>스터디원이 없습니다</li>;
-    }
-
-    if (showAll) {
-      return (
-        <>
-          <li key={owner.id} css={tw`relative`}>
-            <a href={owner.profileUrl}>
-              <div css={tw`absolute top-0 left-20 z-5`}>
-                <CrownIcon />
-              </div>
-              <StudyMemberCard
-                username={owner.username}
-                imageUrl={owner.imageUrl}
-                startDate={changeDateSeperator(owner.participationDate)}
-                studyCount={owner.numberOfStudy}
-              />
-            </a>
-          </li>
-          {members.map(({ id, username, imageUrl, profileUrl, participationDate, numberOfStudy }) => (
-            <li key={id}>
-              <a href={profileUrl}>
-                <StudyMemberCard
-                  username={username}
-                  imageUrl={imageUrl}
-                  startDate={changeDateSeperator(participationDate)}
-                  studyCount={numberOfStudy}
-                />
-              </a>
-            </li>
-          ))}
-        </>
-      );
-    }
-
-    return (
-      <>
-        <li key={owner.id} css={tw`relative`}>
-          <a href={owner.profileUrl}>
-            <div css={tw`absolute top-0 left-20 z-5`}>
-              <CrownIcon />
-            </div>
-            <StudyMemberCard
-              username={owner.username}
-              imageUrl={owner.imageUrl}
-              startDate={changeDateSeperator(owner.participationDate)}
-              studyCount={owner.numberOfStudy}
-            />
-          </a>
-        </li>
-        {members
-          .slice(0, DEFAULT_VISIBLE_STUDY_MEMBER_CARD_COUNT - 1)
-          .map(({ id, username, imageUrl, profileUrl, participationDate, numberOfStudy }) => (
-            <li key={id}>
-              <a href={profileUrl}>
-                <StudyMemberCard
-                  username={username}
-                  imageUrl={imageUrl}
-                  startDate={changeDateSeperator(participationDate)}
-                  studyCount={numberOfStudy}
-                />
-              </a>
-            </li>
-          ))}
-      </>
-    );
   };
 
   return (
@@ -107,22 +42,29 @@ const StudyMemberSection: React.FC<StudyMemberSectionProps> = ({ owner, members 
       <SectionTitle>
         스터디원 <span css={tw`text-[${theme.fontSize.md}]`}>{totalMembers.length}명</span>
       </SectionTitle>
-      <MemberList>{renderMembers()}</MemberList>
-      {totalMembers.length > DEFAULT_VISIBLE_STUDY_MEMBER_CARD_COUNT && (
-        <div css={tw`text-right pt-15 pb-15`}>
-          <MoreButton
-            status={showAll ? 'unfold' : 'fold'}
-            onClick={handleShowMoreButtonClick}
-            foldText="- 접기"
-            unfoldText="+ 더보기"
+      <StudyMemberList>
+        {!hasStudyMembers && <NoStudyMember />}
+        {hasStudyMembers && (
+          <StudyMemberListItems
+            size={showAll ? members.length : DEFAULT_VISIBLE_STUDY_MEMBER_CARD_COUNT}
+            owner={owner}
+            members={members}
           />
-        </div>
+        )}
+      </StudyMemberList>
+      {isOverDefaultMemberCount && (
+        <MoreButton
+          status={showAll ? 'unfold' : 'fold'}
+          onClick={handleShowMoreButtonClick}
+          foldText="- 접기"
+          unfoldText="+ 더보기"
+        />
       )}
     </section>
   );
 };
 
-const MemberList = styled.ul`
+const StudyMemberList = styled.ul`
   display: grid;
   grid-template-columns: repeat(2, minmax(auto, 1fr));
   grid-column-gap: 30px;
@@ -134,5 +76,51 @@ const MemberList = styled.ul`
     row-gap: 20px;
   }
 `;
+
+const NoStudyMember = () => <li>스터디원이 없습니다</li>;
+
+type StudyMemberListItemsProps = { size: number } & StudyMemberSectionProps;
+const StudyMemberListItems: React.FC<StudyMemberListItemsProps> = ({ size, owner, members }) => (
+  <>
+    <li key={owner.id} css={tw`relative`}>
+      <a href={owner.profileUrl}>
+        <div css={tw`absolute top-0 left-20 z-5`}>
+          <CrownIcon />
+        </div>
+        <StudyMemberCard
+          username={owner.username}
+          imageUrl={owner.imageUrl}
+          startDate={changeDateSeperator(owner.participationDate)}
+          studyCount={owner.numberOfStudy}
+        />
+      </a>
+    </li>
+    {members.slice(0, size).map(({ id, username, imageUrl, profileUrl, participationDate, numberOfStudy }) => (
+      <li key={id}>
+        <a href={profileUrl}>
+          <StudyMemberCard
+            username={username}
+            imageUrl={imageUrl}
+            startDate={changeDateSeperator(participationDate)}
+            studyCount={numberOfStudy}
+          />
+        </a>
+      </li>
+    ))}
+  </>
+);
+
+const MoreButton: React.FC<MoreButtonProps> = ({ ...props }) => {
+  const style = css`
+    text-align: right;
+    padding-top: 15px;
+    padding-bottom: 15px;
+  `;
+  return (
+    <div css={style}>
+      <ImportedMoreButton {...props} />
+    </div>
+  );
+};
 
 export default StudyMemberSection;
