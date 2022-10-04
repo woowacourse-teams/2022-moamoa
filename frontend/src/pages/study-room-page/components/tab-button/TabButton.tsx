6ac7c692dc29f38ff2002d7dbd060d35dffb2f73
airman5573 <rpf5573@kakao.com>
2022-10-04 17:59:28 +0900

    feat: TabButton 개선

--- frontend/src/pages/study-room-page/components/tab-button/TabButton.tsx
+++ frontend/src/pages/study-room-page/components/tab-button/TabButton.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from '@emotion/react';
+
 import { ToggleButton } from '@components/button';
 
 export type TabButtonProps = {
@@ -7,8 +9,15 @@ export type TabButtonProps = {
 };
 
 const TabButton: React.FC<TabButtonProps> = ({ children, isSelected, onClick: handleClick }) => {
+  const theme = useTheme();
   return (
-    <ToggleButton checked={isSelected} onClick={handleClick} variant="secondary" fluid fontSize="lg">
+    <ToggleButton
+      checked={isSelected}
+      onClick={handleClick}
+      variant="secondary"
+      fluid
+      custom={{ fontSize: theme.fontSize.lg }}
+    >
       {children}
     </ToggleButton>
   );
