b1da94d6f8335713f082613879cd469282bb2265
airman5573 <rpf5573@kakao.com>
2022-10-04 14:06:39 +0900

    MyStudyPage 개선

--- frontend/src/pages/my-study-page/MyStudyPage.tsx
+++ frontend/src/pages/my-study-page/MyStudyPage.tsx
@@ -1,42 +1,55 @@
+import { MyStudy } from '@custom-types';
+
 import Divider from '@components/divider/Divider';
 import PageTitle from '@components/page-title/PageTitle';
 import Wrapper from '@components/wrapper/Wrapper';
 
 import MyStudyCardListSection from '@my-study-page/components/my-study-card-list-section/MyStudyCardListSection';
-import { useMyStudyPage } from '@my-study-page/hooks/useMyStudyPage';
+import { StudyType, useMyStudyPage } from '@my-study-page/hooks/useMyStudyPage';
 
 const MyStudyPage: React.FC = () => {
   const { myStudyQueryResult, studies } = useMyStudyPage();
 
   const { isFetching, isError, isSuccess } = myStudyQueryResult;
 
-  const renderStudyListSections = () => {
-    if (isFetching) {
-      return <div>로딩 중...</div>;
-    }
-
-    if (isError || !isSuccess) {
-      return <div>내 스터디 불러오기를 실패했습니다</div>;
-    }
-
-    return (
-      <>
-        <MyStudyCardListSection sectionTitle="활동 중!" studies={studies.inProgress} />
-        <Divider space="10px" />
-        <MyStudyCardListSection sectionTitle="곧 시작해요!" studies={studies.prepare} />
-        <Divider space="10px" />
-        <MyStudyCardListSection sectionTitle="종료했어요" studies={studies.done} done />
-      </>
-    );
-  };
-
   return (
     <Wrapper>
       <PageTitle align="center">가입한 스터디 목록</PageTitle>
       <Divider />
-      {renderStudyListSections()}
+      {isFetching && <Loading />}
+      {isError && !isSuccess && <Error />}
+      {!isError && isSuccess && (
+        <>
+          <InProgressStudyList studies={studies} />
+          <Divider space="10px" />
+          <PrepareStudyList studies={studies} />
+          <Divider space="10px" />
+          <DoneStudyList studies={studies} />
+        </>
+      )}
     </Wrapper>
   );
 };
+
+const Loading = () => <div>Loading...</div>;
+
+const Error = () => <div>내 스터디 불러오기를 실패했습니다</div>;
+
+type InProgressStudyListProps = {
+  studies: Record<StudyType, Array<MyStudy>>;
+};
+const InProgressStudyList: React.FC<InProgressStudyListProps> = ({ studies }) => (
+  <MyStudyCardListSection sectionTitle="활동 중!" studies={studies.inProgress} />
+);
+
+type PrepareStudyListProps = InProgressStudyListProps;
+const PrepareStudyList: React.FC<PrepareStudyListProps> = ({ studies }) => (
+  <MyStudyCardListSection sectionTitle="곧 시작해요!" studies={studies.prepare} />
+);
+
+type DoneStudyListProps = InProgressStudyListProps;
+const DoneStudyList: React.FC<DoneStudyListProps> = ({ studies }) => (
+  <MyStudyCardListSection sectionTitle="종료했어요" studies={studies.done} />
+);
 
 export default MyStudyPage;
