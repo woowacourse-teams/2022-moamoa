23c5a8d195a189d45e5e1a6d356a6ffb5fbaebd5
airman5573 <rpf5573@kakao.com>
2022-10-04 20:21:51 +0900

    feat: ArticleList 개선

--- frontend/src/pages/study-room-page/tabs/community-tab-panel/components/article-list/ArticleList.tsx
+++ frontend/src/pages/study-room-page/tabs/community-tab-panel/components/article-list/ArticleList.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 import { PATH } from '@constants';
 
-import type { StudyId } from '@custom-types';
+import type { CommunityArticle, StudyId } from '@custom-types';
 
 import { useGetCommunityArticles } from '@api/community';
 
@@ -21,41 +21,47 @@ const ArticleList: React.FC<ArticleListProps> = ({ studyId }) => {
   const [page, setPage] = useState<number>(1);
   const { isFetching, isSuccess, isError, data } = useGetCommunityArticles({ studyId, page });
 
-  if (isFetching) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError || !isSuccess) {
-    return <div>에러가 발생했습니다</div>;
-  }
-
-  const { articles, lastPage, currentPage } = data;
-
-  if (articles.length === 0) {
-    return <div>게시글이 없습니다</div>;
-  }
-
   return (
     <Flex flexDirection="column" rowGap="20px">
-      <ul>
-        {articles.map(article => (
-          <li key={article.id}>
-            <Link to={PATH.COMMUNITY_ARTICLE(article.id)}>
-              <ArticleListItem title={article.title} author={article.author} createdDate={article.createdDate} />
-            </Link>
-            <Divider />
-          </li>
-        ))}
-      </ul>
-      <Pagination
-        count={lastPage - 1}
-        defaultPage={currentPage}
-        onNumberButtonClick={num => {
-          setPage(num);
-        }}
-      />
+      {isFetching && <Loading />}
+      {isError && <Error />}
+      {isSuccess && data.articles && data.articles.length === 0 && <NoArticle />}
+      {isSuccess && (
+        <>
+          <Self articles={data.articles} />
+          <Pagination
+            count={data.lastPage - 1}
+            defaultPage={data.currentPage}
+            onNumberButtonClick={num => {
+              setPage(num);
+            }}
+          />
+        </>
+      )}
     </Flex>
   );
 };
+
+const Loading = () => <div>Loading...</div>;
+
+const Error = () => <div>에러가 발생했습니다</div>;
+
+const NoArticle = () => <div>게시글이 없습니다</div>;
+
+type SelfProps = {
+  articles: Array<CommunityArticle>;
+};
+const Self: React.FC<SelfProps> = ({ articles }) => (
+  <ul>
+    {articles.map(article => (
+      <li key={article.id}>
+        <Link to={PATH.COMMUNITY_ARTICLE(article.id)}>
+          <ArticleListItem title={article.title} author={article.author} createdDate={article.createdDate} />
+        </Link>
+        <Divider />
+      </li>
+    ))}
+  </ul>
+);
 
 export default ArticleList;
