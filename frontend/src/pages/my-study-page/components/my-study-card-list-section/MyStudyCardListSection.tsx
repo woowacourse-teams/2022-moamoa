729ad82e585219fa1b245be811d85249e1efba15
airman5573 <rpf5573@kakao.com>
2022-10-04 14:33:56 +0900

    feat: MyStudyCardListSection 개선

--- frontend/src/pages/my-study-page/components/my-study-card-list-section/MyStudyCardListSection.tsx
+++ frontend/src/pages/my-study-page/components/my-study-card-list-section/MyStudyCardListSection.tsx
@@ -2,6 +2,7 @@ import { AxiosError } from 'axios';
 import { useQueryClient } from 'react-query';
 import { Link } from 'react-router-dom';
 
+import { Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { PATH } from '@constants';
@@ -27,14 +28,20 @@ export type MyStudyCardListSectionProps = {
   done?: boolean;
 };
 
+type HandleQuitButtonClickHandler = ({
+  title,
+  id,
+  owner,
+}: Pick<MyStudy, 'title' | 'id' | 'owner'>) => (e: React.MouseEvent<HTMLButtonElement>) => void;
+
 const MyStudyCardListSection: React.FC<MyStudyCardListSectionProps> = ({ sectionTitle, studies, done = false }) => {
   const queryClient = useQueryClient();
   const { mutate } = useDeleteMyStudy();
   const { userInfo } = useUserInfo();
 
-  const handleTrashButtonClick =
-    ({ title, id, owner }: Pick<MyStudy, 'title' | 'id' | 'owner'>) =>
-    (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleTrashButtonClick: HandleQuitButtonClickHandler =
+    ({ title, id, owner }) =>
+    e => {
       e.stopPropagation();
 
       if (userInfo.id === owner.id) {
@@ -60,32 +67,22 @@ const MyStudyCardListSection: React.FC<MyStudyCardListSectionProps> = ({ section
     };
 
   return (
-    <section css={tw`p-8`}>
+    <Self>
       <SectionTitle>{sectionTitle}</SectionTitle>
       <MyStudyList>
         {studies.length === 0 ? (
-          <li>해당하는 스터디가 없습니다</li>
+          <NoResult />
         ) : (
-          studies.map(study => (
-            <li key={study.id}>
-              <Link to={PATH.STUDY_ROOM(study.id)}>
-                <MyStudyCard
-                  title={study.title}
-                  ownerName={study.owner.username}
-                  tags={study.tags}
-                  startDate={study.startDate}
-                  endDate={study.endDate}
-                  done={done}
-                  onQuitStudyButtonClick={handleTrashButtonClick(study)}
-                />
-              </Link>
-            </li>
-          ))
+          <MyStudyCardLinkItemList studies={studies} done={done} onQuitButtonClick={handleTrashButtonClick} />
         )}
       </MyStudyList>
-    </section>
+    </Self>
   );
 };
+
+const Self = styled.section`
+  padding: 8px;
+`;
 
 const MyStudyList = styled.ul`
   display: grid;
@@ -104,5 +101,36 @@ const MyStudyList = styled.ul`
     grid-template-columns: repeat(1, minmax(auto, 1fr));
   }
 `;
+
+const NoResult = () => <li>해당하는 스터디가 없습니다</li>;
+
+type MyStudyCardLinkItemListProps = {
+  studies: MyStudyCardListSectionProps['studies'];
+  done: MyStudyCardListSectionProps['done'];
+  onQuitButtonClick: HandleQuitButtonClickHandler;
+};
+const MyStudyCardLinkItemList: React.FC<MyStudyCardLinkItemListProps> = ({
+  studies,
+  done,
+  onQuitButtonClick: handleQuitButtonClick,
+}) => (
+  <>
+    {studies.map(study => (
+      <li key={study.id}>
+        <Link to={PATH.STUDY_ROOM(study.id)}>
+          <MyStudyCard
+            title={study.title}
+            ownerName={study.owner.username}
+            tags={study.tags}
+            startDate={study.startDate}
+            endDate={study.endDate}
+            done={done}
+            onQuitStudyButtonClick={handleQuitButtonClick(study)}
+          />
+        </Link>
+      </li>
+    ))}
+  </>
+);
 
 export default MyStudyCardListSection;
