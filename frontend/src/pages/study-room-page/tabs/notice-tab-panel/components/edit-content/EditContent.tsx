ca22c376d0f97bbe2fe3c543dc99b99b245a4ccc
airman5573 <rpf5573@kakao.com>
2022-10-04 21:36:20 +0900

    feat: EditContent 개선

--- frontend/src/pages/study-room-page/tabs/notice-tab-panel/components/edit-content/EditContent.tsx
+++ frontend/src/pages/study-room-page/tabs/notice-tab-panel/components/edit-content/EditContent.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from 'react';
 
+import { css } from '@emotion/react';
+
 import { DESCRIPTION_LENGTH } from '@constants';
 
 import tw from '@utils/tw';
 
-import { makeValidationResult, useFormContext } from '@hooks/useForm';
+import { UseFormRegister, makeValidationResult, useFormContext } from '@hooks/useForm';
 
 import { ToggleButton } from '@components/button';
 import ButtonGroup from '@components/button-group/ButtonGroup';
@@ -52,40 +54,7 @@ const EditContent: React.FC<EditContentProps> = ({ content }) => {
     setDescription(description);
   }, [activeTab]);
 
-  const renderTabContent = () => {
-    const isWriteTab = activeTab === tabMode.write;
-
-    return (
-      <>
-        <div css={isWriteTab ? tw`h-full` : tw`hidden`}>
-          <Label htmlFor={CONTENT} hidden>
-            소개글
-          </Label>
-          <Textarea
-            id={CONTENT}
-            placeholder={`게시글 내용 (${DESCRIPTION_LENGTH.MAX.VALUE}자 제한)`}
-            invalid={!isValid}
-            defaultValue={content}
-            {...register(CONTENT, {
-              validate: (val: string) => {
-                if (val.length < DESCRIPTION_LENGTH.MIN.VALUE) {
-                  return makeValidationResult(true, DESCRIPTION_LENGTH.MIN.MESSAGE);
-                }
-                return makeValidationResult(false);
-              },
-              validationMode: 'change',
-              minLength: DESCRIPTION_LENGTH.MIN.VALUE,
-              maxLength: DESCRIPTION_LENGTH.MAX.VALUE,
-              required: true,
-            })}
-          ></Textarea>
-        </div>
-        <div css={isWriteTab && tw`hidden`}>
-          <MarkdownRender markdownContent={description} />
-        </div>
-      </>
-    );
-  };
+  const isWriteTab = activeTab === tabMode.write;
 
   return (
     <MetaBox>
@@ -96,7 +65,14 @@ const EditContent: React.FC<EditContentProps> = ({ content }) => {
         </ButtonGroup>
       </MetaBox.Title>
       <MetaBox.Content>
-        <div css={tw`h-[50vh]`}>{renderTabContent()}</div>
+        <div
+          css={css`
+            height: 50vh;
+          `}
+        >
+          <WriteTab isOpen={isWriteTab} isValid={isValid} defaultValue={content} register={register} />
+          <MarkdownRendererTab isOpen={!isWriteTab} description={description} />
+        </div>
       </MetaBox.Content>
     </MetaBox>
   );
@@ -118,5 +94,57 @@ const PreviewTabButton: React.FC<PreviewTabButtonProps> = ({ activeTab, onClick:
     Preview
   </ToggleButton>
 );
+
+type WriteTabProps = {
+  isOpen: boolean;
+  isValid: boolean;
+  defaultValue: string;
+  register: UseFormRegister;
+};
+const WriteTab: React.FC<WriteTabProps> = ({ isOpen, isValid, defaultValue, register }) => {
+  const style = css`
+    display: ${isOpen ? 'block' : 'none'};
+  `;
+  return (
+    <div css={style}>
+      <Label htmlFor={CONTENT} hidden>
+        소개글
+      </Label>
+      <Textarea
+        id={CONTENT}
+        placeholder={`게시글 내용 (${DESCRIPTION_LENGTH.MAX.VALUE}자 제한)`}
+        invalid={!isValid}
+        defaultValue={defaultValue}
+        {...register(CONTENT, {
+          validate: (val: string) => {
+            if (val.length < DESCRIPTION_LENGTH.MIN.VALUE) {
+              return makeValidationResult(true, DESCRIPTION_LENGTH.MIN.MESSAGE);
+            }
+            return makeValidationResult(false);
+          },
+          validationMode: 'change',
+          minLength: DESCRIPTION_LENGTH.MIN.VALUE,
+          maxLength: DESCRIPTION_LENGTH.MAX.VALUE,
+          required: true,
+        })}
+      ></Textarea>
+    </div>
+  );
+};
+
+type MarkdownRendererTabProps = {
+  isOpen: boolean;
+  description: string;
+};
+const MarkdownRendererTab: React.FC<MarkdownRendererTabProps> = ({ isOpen, description }) => {
+  const style = css`
+    display: ${isOpen ? 'block' : 'none'};
+  `;
+  return (
+    <div css={style}>
+      <MarkdownRender markdownContent={description} />
+    </div>
+  );
+};
 
 export default EditContent;
