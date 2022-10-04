caa4f9efe4b521992c39810471e366ff93ce6d6a
airman5573 <rpf5573@kakao.com>
2022-10-04 17:17:56 +0900

    feat: Title 개선

--- frontend/src/pages/study-page/components/title/Title.tsx
+++ frontend/src/pages/study-page/components/title/Title.tsx
@@ -1,14 +1,19 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
 import { TITLE_LENGTH } from '@constants';
 
 import tw from '@utils/tw';
 
 import type { StudyDetail } from '@custom-types';
 
-import { type FieldElement, makeValidationResult, useFormContext } from '@hooks/useForm';
+import { type FieldElement, UseFormRegister, makeValidationResult, useFormContext } from '@hooks/useForm';
 
 import Input from '@components/input/Input';
 import Label from '@components/label/Label';
-import LetterCounter from '@components/letter-counter/LetterCounter';
+import ImportedLetterCounter, {
+  type LetterCounterProps as ImportedLetterCounterProps,
+} from '@components/letter-counter/LetterCounter';
 import useLetterCount from '@components/letter-counter/useLetterCount';
 
 export type TitleProps = {
@@ -27,42 +32,75 @@ const Title: React.FC<TitleProps> = ({ originalTitle }) => {
   const handleTitleChange = ({ target: { value } }: React.ChangeEvent<FieldElement>) => setCount(value.length);
 
   return (
-    <div css={tw`relative`}>
-      <div css={tw`absolute right-4 bottom-2`}>
-        <LetterCounter count={count} maxCount={maxCount} />
-      </div>
-      <Label htmlFor={TITLE} hidden>
-        스터디 이름
-      </Label>
-      <div css={tw`mb-20`}>
-        <Input
-          id={TITLE}
-          type="text"
-          placeholder="*스터디 이름"
-          invalid={!isValid}
-          fluid
-          defaultValue={originalTitle}
-          fontSize="xl"
-          {...register(TITLE, {
-            validate: (val: string) => {
-              if (val.length < TITLE_LENGTH.MIN.VALUE) {
-                return makeValidationResult(true, TITLE_LENGTH.MIN.MESSAGE);
-              }
-              if (val.length > TITLE_LENGTH.MAX.VALUE) {
-                return makeValidationResult(true, TITLE_LENGTH.MAX.MESSAGE);
-              }
-              return makeValidationResult(false);
-            },
-            validationMode: 'change',
-            onChange: handleTitleChange,
-            minLength: TITLE_LENGTH.MIN.VALUE,
-            maxLength: TITLE_LENGTH.MAX.VALUE,
-            required: true,
-          })}
-        />
-      </div>
+    <Self>
+      <LetterCounter count={count} maxCount={maxCount} />
+      <StudyNameField
+        isValid={isValid}
+        defaultValue={originalTitle ?? ''}
+        onChange={handleTitleChange}
+        register={register}
+      />
+    </Self>
+  );
+};
+
+const Self = styled.div`
+  position: relative;
+`;
+
+type LetterCouterProps = ImportedLetterCounterProps;
+const LetterCounter: React.FC<LetterCouterProps> = ({ ...props }) => {
+  const style = css`
+    position: absolute;
+    right: 4px;
+    bottom: 2px;
+  `;
+  return (
+    <div css={style}>
+      <ImportedLetterCounter {...props} />
     </div>
   );
 };
+
+type StudyNameFieldProps = {
+  isValid: boolean;
+  defaultValue: string;
+  register: UseFormRegister;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
+const StudyNameField: React.FC<StudyNameFieldProps> = ({ isValid, defaultValue, register, onChange: handleChange }) => (
+  <>
+    <Label htmlFor={TITLE} hidden>
+      스터디 이름
+    </Label>
+    <div css={tw`mb-20`}>
+      <Input
+        id={TITLE}
+        type="text"
+        placeholder="*스터디 이름"
+        invalid={!isValid}
+        fluid
+        defaultValue={defaultValue}
+        fontSize="xl"
+        {...register(TITLE, {
+          validate: (val: string) => {
+            if (val.length < TITLE_LENGTH.MIN.VALUE) {
+              return makeValidationResult(true, TITLE_LENGTH.MIN.MESSAGE);
+            }
+            if (val.length > TITLE_LENGTH.MAX.VALUE) {
+              return makeValidationResult(true, TITLE_LENGTH.MAX.MESSAGE);
+            }
+            return makeValidationResult(false);
+          },
+          validationMode: 'change',
+          onChange: handleChange,
+          minLength: TITLE_LENGTH.MIN.VALUE,
+          maxLength: TITLE_LENGTH.MAX.VALUE,
+          required: true,
+        })}
+      />
+    </div>
+  </>
+);
 
 export default Title;
