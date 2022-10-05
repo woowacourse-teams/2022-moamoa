c2e871f4ca16360a0aa158f0bc1a20b64afeb298
airman5573 <rpf5573@kakao.com>
2022-10-05 12:02:47 +0900

    feat: ReviewTabPanel 개선

--- frontend/src/pages/study-room-page/tabs/review-tab-panel/ReviewTabPanel.tsx
+++ frontend/src/pages/study-room-page/tabs/review-tab-panel/ReviewTabPanel.tsx
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { useGetStudyReviews } from '@api/reviews';
+import { Member, StudyId, StudyReview } from '@custom-types';
+
+import { ApiReviews, useGetStudyReviews } from '@api/reviews';
 
 import { useUserInfo } from '@hooks/useUserInfo';
 
@@ -18,6 +20,11 @@ const ReviewTabPanel: React.FC = () => {
   const { data, isFetching, refetch, isError, isSuccess } = useGetStudyReviews({ studyId });
   const { userInfo, fetchUserInfo } = useUserInfo();
 
+  const reviews = data?.reviews;
+  const noReview = !isFetching && isSuccess && reviews && reviews.length === 0;
+  const hasReview = (val: unknown): val is Array<StudyReview> =>
+    !isFetching && isSuccess && !!reviews && reviews.length > 0;
+
   useEffect(() => {
     fetchUserInfo();
   }, []);
@@ -31,47 +38,47 @@ const ReviewTabPanel: React.FC = () => {
     alert('댓글 입력에 오류가 발생했습니다');
   };
 
-  const renderReviewList = () => {
-    if (isFetching) {
-      return <div>불러오는중...</div>;
-    }
-
-    if (isError || !isSuccess) {
-      return <div>에러가 발생했습니다</div>;
-    }
-
-    if (data.reviews.length === 0) {
-      return <div>첫 리뷰를 남겨주세요!</div>;
-    }
-
-    return (
-      <>
-        {data.reviews.map(review => (
-          <>
-            <li key={review.id}>
-              <ReviewComment
-                id={review.id}
-                studyId={studyId}
-                author={review.member}
-                date={review.createdDate}
-                content={review.content}
-                isMyComment={userInfo.id === review.member.id}
-              />
-            </li>
-            <Divider space="30px" />
-          </>
-        ))}
-      </>
-    );
-  };
-
   return (
     <Wrapper>
       <ReviewForm author={userInfo} studyId={studyId} onPostSuccess={handlePostSuccess} onPostError={handlePostError} />
       <Divider space="30px" />
-      {renderReviewList()}
+      {isFetching && <Loading />}
+      {isError && !isSuccess && <Error />}
+      {noReview && <NoReview />}
+      {hasReview(reviews) && <ReviewList reviews={reviews} studyId={studyId} userInfo={userInfo} />}
     </Wrapper>
   );
 };
+
+const Loading = () => <div>Loading...</div>;
+
+const Error = () => <div>에러가 발생했습니다</div>;
+
+const NoReview = () => <div>첫 리뷰를 남겨주세요!</div>;
+
+type ReviewListProps = {
+  reviews: Array<StudyReview>;
+  studyId: StudyId;
+  userInfo: Member;
+};
+const ReviewList: React.FC<ReviewListProps> = ({ reviews, studyId, userInfo }) => (
+  <ul>
+    {reviews.map(review => (
+      <>
+        <li key={review.id}>
+          <ReviewComment
+            id={review.id}
+            studyId={studyId}
+            author={review.member}
+            date={review.createdDate}
+            content={review.content}
+            isMyComment={userInfo.id === review.member.id}
+          />
+        </li>
+        <Divider space="30px" />
+      </>
+    ))}
+  </ul>
+);
 
 export default ReviewTabPanel;
