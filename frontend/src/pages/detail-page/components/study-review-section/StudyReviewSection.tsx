d7f6b6ad615c01d6651ec96f0e724fbd316b5a56
airman5573 <rpf5573@kakao.com>
2022-10-05 13:30:07 +0900

    feat: StudyReviewSection 개선

--- frontend/src/pages/detail-page/components/study-review-section/StudyReviewSection.tsx
+++ frontend/src/pages/detail-page/components/study-review-section/StudyReviewSection.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 import { DEFAULT_LOAD_STUDY_REVIEW_COUNT } from '@constants';
 
 import tw from '@utils/tw';
+
+import { StudyReview } from '@custom-types';
 
 import { mqDown } from '@styles/responsive';
 import { theme } from '@styles/theme';
@@ -13,7 +16,9 @@ import { useGetStudyReviews } from '@api/reviews';
 
 import SectionTitle from '@components/section-title/SectionTitle';
 
-import MoreButton from '@detail-page/components/more-button/MoreButton';
+import ImportedMoreButton, {
+  type MoreButtonProps as ImportedMoreButtonProps,
+} from '@detail-page/components/more-button/MoreButton';
 import StudyReviewCard from '@detail-page/components/study-review-card/StudyReviewCard';
 
 export type StudyReviewSectionProps = {
@@ -26,49 +31,69 @@ const StudyReviewSection: React.FC<StudyReviewSectionProps> = ({ studyId }) => {
   const size = showAll ? undefined : DEFAULT_LOAD_STUDY_REVIEW_COUNT;
   const { data, isError, isFetching, isSuccess } = useGetStudyReviews({ studyId: Number(studyId), size });
 
+  const reviews = data?.reviews;
+  const noReview = !isFetching && isSuccess && reviews && reviews.length === 0;
+  const hasReview = (val: unknown): val is Array<StudyReview> =>
+    !isFetching && isSuccess && !!reviews && reviews.length > 0;
+
   const handleMoreButtonClick = () => {
     setIsMoreButtonVisible(prev => !prev);
   };
 
-  const renderReviews = () => {
-    if (isFetching) return <div>로딩중...</div>;
-    if (!isSuccess || isError) return <div>후기 불러오기를 실패했습니다</div>;
-    if (data.reviews.length === 0) return <div>아직 작성된 후기가 없습니다</div>;
-    return (
-      <>
-        <ReviewList>
-          {data.reviews.map(review => (
-            <ReviewListItem key={review.id}>
-              <StudyReviewCard
-                imageUrl={review.member.imageUrl}
-                username={review.member.username}
-                reviewDate={review.createdDate}
-                review={review.content}
-              />
-            </ReviewListItem>
-          ))}
-        </ReviewList>
-        {data.reviews.length >= DEFAULT_LOAD_STUDY_REVIEW_COUNT && (
-          <div css={tw`p-16 text-right`}>
+  return (
+    <Self>
+      <SectionTitle>
+        후기
+        {
+          <span
+            css={css`
+              font-size: ${theme.fontSize.md};
+            `}
+          >
+            {data?.totalCount ?? '0'}개
+          </span>
+        }
+      </SectionTitle>
+      {isFetching && <Loading />}
+      {isError && !isSuccess && <Error />}
+      {noReview && <NoReview />}
+      {hasReview(reviews) && (
+        <>
+          <ReviewList>
+            {reviews.map(review => (
+              <ReviewListItem key={review.id}>
+                <StudyReviewCard
+                  imageUrl={review.member.imageUrl}
+                  username={review.member.username}
+                  reviewDate={review.createdDate}
+                  review={review.content}
+                />
+              </ReviewListItem>
+            ))}
+          </ReviewList>
+          {reviews.length >= DEFAULT_LOAD_STUDY_REVIEW_COUNT && (
             <MoreButton
               status={showAll ? 'unfold' : 'fold'}
               onClick={handleMoreButtonClick}
               foldText="- 접기"
               unfoldText="+ 더보기"
             />
-          </div>
-        )}
-      </>
-    );
-  };
-
-  return (
-    <section css={tw`p-16`}>
-      <SectionTitle>후기 {<span css={tw`text-[${theme.fontSize.md}]`}>{data?.totalCount ?? '0'}개</span>}</SectionTitle>
-      {renderReviews()}
-    </section>
+          )}
+        </>
+      )}
+    </Self>
   );
 };
+
+const Self = styled.section`
+  padding: 16px;
+`;
+
+const Loading = () => <div>로딩중...</div>;
+
+const Error = () => <div>후기 불러오기를 실패했습니다</div>;
+
+const NoReview = () => <div>아직 작성된 후기가 없습니다</div>;
 
 const ReviewList = styled.ul`
   display: flex;
@@ -81,12 +106,24 @@ const ReviewList = styled.ul`
   }
 `;
 
-export const ReviewListItem = styled.li`
+const ReviewListItem = styled.li`
   width: calc(50% - 30px);
 
   ${mqDown('md')} {
     width: 100%;
   }
 `;
+
+type MoreButtonProps = ImportedMoreButtonProps;
+const MoreButton: React.FC<MoreButtonProps> = ({ ...props }) => (
+  <div
+    css={css`
+      padding: 16px;
+      text-align: right;
+    `}
+  >
+    <ImportedMoreButton {...props} />
+  </div>
+);
 
 export default StudyReviewSection;
