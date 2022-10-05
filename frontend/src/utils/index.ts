4171b70dde491c3269779a4ecacf2d193d809c9b
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-10-05 21:07:46 +0900

    feat: typeChecker 구현

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,5 +1,6 @@
 export { default as getRandomInt } from '@utils/getRandomInt';
 export { default as noop } from '@utils/noop';
-export { mqBetween, mqDown, mqUp } from '@utils/media-query';
-export { changeDateSeperator, getNextYear, getToday, yyyymmddTommdd } from '@utils/dates';
-export { isFunction, isNullOrUndefined, isNumber, isObject, isString } from '@utils/typeChecker';
+export * from '@utils/media-query';
+export * from '@utils/dates';
+export * from '@utils/typeChecker';
+export { default as checkType } from '@utils/typeChecker';
--- frontend/src/utils/typeChecker.ts
+++ frontend/src/utils/typeChecker.ts
@@ -1,4 +1,12 @@
+import { AxiosError } from 'axios';
+
+import type { CategoryName, DateYMD, RecruitmentStatus, StudyStatus, UserRole } from '@custom-types';
+
+export const isNull = (value: unknown): value is null => value === null;
+
 export const isNullOrUndefined = (value: unknown): value is null | undefined => value === undefined || value === null;
+
+export const isBoolean = (value: unknown): value is boolean => typeof value === 'boolean';
 
 export const isString = (value: unknown): value is string => typeof value === 'string';
 
@@ -6,7 +14,55 @@ export const isNumber = (value: unknown): value is number => typeof value === 'n
 
 export const isFunction = (val: unknown): val is (...args: any) => any => typeof val === 'function';
 
-const isObjectType = (value: unknown) => typeof value === 'object';
-
 export const isObject = <T extends object>(value: unknown): value is T =>
-  !isNullOrUndefined(value) && !Array.isArray(value) && isObjectType(value);
+  typeof value === 'object' && !isNullOrUndefined(value) && !Array.isArray(value);
+
+export const isArray = (value: unknown): value is Array<unknown> => Array.isArray(value);
+
+export const isDateYMD = (value: unknown): value is DateYMD => {
+  if (!isString(value)) return false;
+  const regex = /\d{4}-\d{2}-\d{2}/;
+  return regex.test(value);
+};
+
+export const isUserRole = (value: unknown): value is UserRole =>
+  isString(value) && ['OWNER', 'MEMBER', 'NON_MEMBER'].includes(value);
+
+export const isStudyStatus = (value: unknown): value is StudyStatus =>
+  isString(value) && ['PREPARE', 'IN_PROGRESS', 'DONE'].includes(value);
+
+export const isRecruitmentStatus = (value: unknown): value is RecruitmentStatus =>
+  isString(value) && ['RECRUITMENT_START', 'RECRUITMENT_END'].includes(value);
+
+export const isCategoryName = (value: unknown): value is CategoryName =>
+  isString(value) && ['generation', 'area', 'subject'].includes(value);
+
+export const hasOwnProperty = <X extends object, Y extends PropertyKey>(
+  obj: X,
+  prop: Y,
+): obj is X & Record<Y, unknown> => obj.hasOwnProperty(prop);
+
+export const hasOwnProperties = <X extends object, Y extends PropertyKey>(
+  obj: X,
+  props: Array<Y>,
+): obj is X & Record<Y, unknown> => props.every(prop => obj.hasOwnProperty(prop));
+
+export const checkOptionalType = <T>(value: unknown, isTypeFn: (value: unknown) => value is T): T | undefined => {
+  if (!isTypeFn(value) && !isNullOrUndefined(value)) {
+    console.error(`${isTypeFn} ${value} does not have correct type`);
+    throw new AxiosError(`${value} does not have correct type`);
+  }
+
+  return value ?? undefined;
+};
+
+const checkType = <T>(value: unknown, isTypeFn: (value: unknown) => value is T): T => {
+  if (!isTypeFn(value)) {
+    console.error(`${isTypeFn} ${value} does not have correct type`);
+    throw new AxiosError(`${value} does not have correct type`);
+  }
+
+  return value;
+};
+
+export default checkType;
