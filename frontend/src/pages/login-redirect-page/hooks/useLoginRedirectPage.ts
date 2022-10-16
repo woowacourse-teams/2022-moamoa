18b0d2990f8ec4ca503fc98f034ea26726fce035
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-10-16 17:29:52 +0900

    refactor: eslint 적용

--- frontend/src/pages/login-redirect-page/hooks/useLoginRedirectPage.ts
+++ frontend/src/pages/login-redirect-page/hooks/useLoginRedirectPage.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 import { PATH } from '@constants';
 
