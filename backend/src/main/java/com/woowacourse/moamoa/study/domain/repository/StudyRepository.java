8a0249df45327d1668c4d54e01e93d74be574365
jaejae-yoo <wotj102@gmail.com>
2022-10-17 14:46:48 +0900

    refactor: pessimistic lock 적용

--- backend/src/main/java/com/woowacourse/moamoa/study/domain/repository/StudyRepository.java
+++ backend/src/main/java/com/woowacourse/moamoa/study/domain/repository/StudyRepository.java
@@ -3,11 +3,14 @@ package com.woowacourse.moamoa.study.domain.repository;
 import com.woowacourse.moamoa.study.domain.Study;
 import java.util.List;
 import java.util.Optional;
+import javax.persistence.LockModeType;
+import org.springframework.data.jpa.repository.Lock;
 
 public interface StudyRepository {
 
     Study save(Study study);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Study> findById(Long id);
 
     List<Study> findAll();
--- backend/src/main/java/com/woowacourse/moamoa/study/service/StudyParticipantService.java
+++ backend/src/main/java/com/woowacourse/moamoa/study/service/StudyParticipantService.java
@@ -21,7 +21,7 @@ public class StudyParticipantService {
     private final StudyRepository studyRepository;
     private final DateTimeSystem dateTimeSystem;
 
-    public synchronized void participateStudy(final Long memberId, final Long studyId) {
+    public void participateStudy(final Long memberId, final Long studyId) {
         memberRepository.findById(memberId)
                 .orElseThrow(MemberNotFoundException::new);
         final Study study = studyRepository.findById(studyId)
