e88964b9ca78bce3ac3c3dc6cadaba6c67412735
Donggyu <a29661498@gmail.com>
2022-10-19 20:52:52 +0900

    Merge branch 'develop' of https://github.com/woowacourse-teams/2022-moamoa into feat/343-comment
    # Conflicts:
    #	backend/src/test/java/com/woowacourse/acceptance/steps/StudyRelatedSteps.java

--- backend/src/main/java/com/woowacourse/moamoa/comment/service/response/AuthorResponse.java
+++ backend/src/main/java/com/woowacourse/moamoa/comment/service/response/AuthorResponse.java
@@ -1,5 +1,7 @@
 package com.woowacourse.moamoa.comment.service.response;
 
+import static lombok.AccessLevel.PRIVATE;
+
 import com.woowacourse.moamoa.member.query.data.MemberData;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -7,10 +9,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
 @Getter
 @EqualsAndHashCode
-@NoArgsConstructor
+@NoArgsConstructor(access = PRIVATE)
 @ToString
 public class AuthorResponse {
 
