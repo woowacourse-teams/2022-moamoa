4410f3c87206274a791fe3fdffa28fb552a3bbb0
nan-noo <54002105+nan-noo@users.noreply.github.com>
2022-10-20 20:09:40 +0900

    fix: 새로고침 후 유저 역할이 업데이트되지 않는 오류 수정

--- frontend/src/hooks/useUserRole.ts
+++ frontend/src/hooks/useUserRole.ts
@@ -8,11 +8,7 @@ import { useGetUserRole } from '@api/member';
 
 import { useAuth } from '@hooks/useAuth';
 
-import { UserRoleContext } from '@context/userRole/UserRoleProvider';
-
 export const useUserRole = ({ studyId }: { studyId: StudyId }) => {
-  const { userRole, setUserRole } = useContext(UserRoleContext);
-
   const { isLoggedIn } = useAuth();
 
   const {
@@ -28,11 +24,7 @@ export const useUserRole = ({ studyId }: { studyId: StudyId }) => {
     },
   });
 
-  // TODO: 이 훅을 사용하는 페이지에서 새로고침했을 때 setUserRole이 반영되지 않아서 이전 userRole 값을 사용하게 됨
-  useEffect(() => {
-    if (isFetching || isError || !isSuccess) return;
-    setUserRole(data.role);
-  }, [isFetching]);
+  const userRole = data?.role;
 
   return {
     fetchUserRole,
