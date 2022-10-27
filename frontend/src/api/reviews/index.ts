939c4cdcec11147e6d030a56aee9cdaaf970b300
airman5573 <rpf5573@kakao.com>
2022-10-28 01:31:24 +0900

    refactor: 불필요한 함수 제거

--- frontend/src/api/reviews/index.ts
+++ frontend/src/api/reviews/index.ts
@@ -16,20 +16,17 @@ export type ApiStudyReviews = {
     params: {
       studyId: StudyId;
       size?: Size;
+      page?: Page;
     };
     responseData: {
       reviews: Array<StudyReview>;
       totalCount: number;
+      // page의 유무에 따라 hasNext의 존재 유무가 결정된다
+      // TODO: review가 detail페이지에서 한번에 불러오고 있기 때문에 이렇게 hasNext가 optional인데
+      // modal로 UI를 바꿔서 페이지네이션 처리를 해야한다.
+      hasNext?: boolean;
     };
     variables: ApiStudyReviews['get']['params'];
-  };
-};
-
-export type ApiInfiniteStudyReviews = {
-  get: {
-    params: Merge<ApiStudyReviews['get']['params'], { size?: Size }>;
-    responseData: Merge<ApiStudyReviews['get']['responseData'], { hasNext: boolean; totalCount: number }>;
-    variables: ApiInfiniteStudyReviews['get']['params'];
   };
 };
 
@@ -42,32 +39,33 @@ const defaultParam: PageParam = {
 };
 
 // size와 page가 없는 경우에는 전체를 불러온다
-export const getStudyReviews = async ({ studyId, size }: ApiStudyReviews['get']['variables']) => {
-  const url = size ? `/api/studies/${studyId}/reviews?size=${size}` : `/api/studies/${studyId}/reviews`;
+export const getStudyReviews = async ({ studyId, size, page }: ApiStudyReviews['get']['variables']) => {
+  let url = `/api/studies/${studyId}/reviews`;
+  if (size) {
+    url = `/api/studies/${studyId}/reviews?size=${size}`;
+    if (page) `/api/studies/${studyId}/reviews?size=${size}&page=${page}`;
+  }
   const response = await axiosInstance.get<ApiStudyReviews['get']['responseData']>(url);
   return checkStudyReviews(response.data);
 };
-export const useGetStudyReviews = ({ size, studyId }: ApiStudyReviews['get']['variables']) => {
-  return useQuery([QK_STUDY_REVIEWS, size, studyId], () => getStudyReviews({ size, studyId }));
+export const useGetStudyReviews = ({ size, studyId, page }: ApiStudyReviews['get']['variables']) => {
+  return useQuery([QK_STUDY_REVIEWS, size, studyId], () => getStudyReviews({ size, studyId, page }));
 };
 
 export const getStudyReviewsWithPage =
-  ({ size = SIZE, studyId }: ApiInfiniteStudyReviews['get']['variables']) =>
-  async ({
-    pageParam = defaultParam,
-  }): Promise<Merge<ApiInfiniteStudyReviews['get']['responseData'], { page: Page }>> => {
-    const url = `/api/studies/${studyId}/reviews?size=${size}&page=${pageParam.page}`;
-    const response = await axiosInstance.get<ApiInfiniteStudyReviews['get']['responseData']>(url);
-    return { ...response.data, page: pageParam.page };
+  ({ size = SIZE, studyId, page }: ApiStudyReviews['get']['variables']) =>
+  async ({ pageParam = defaultParam }): Promise<Merge<ApiStudyReviews['get']['responseData'], { page: Page }>> => {
+    const data = await getStudyReviews({ studyId, size, page });
+    return { ...data, page: pageParam.page };
   };
 
-export const useGetInfiniteStudyReviews = ({ size, studyId }: ApiInfiniteStudyReviews['get']['variables']) => {
-  return useInfiniteQuery<Merge<ApiInfiniteStudyReviews['get']['responseData'], { page: Page }>, AxiosError>(
+export const useGetInfiniteStudyReviews = ({ size, studyId, page }: ApiStudyReviews['get']['variables']) => {
+  return useInfiniteQuery<Merge<ApiStudyReviews['get']['responseData'], { page: Page; hasNext?: boolean }>, AxiosError>(
     [QK_STUDY_REVIEWS_INFINITE_SCROLL, studyId],
-    getStudyReviewsWithPage({ size, studyId }),
+    getStudyReviewsWithPage({ size, studyId, page }),
     {
       getNextPageParam: (lastPage): NextPageParam => {
-        if (!lastPage.hasNext) return;
+        if (!lastPage.hasNext) return; // 운좋게 undefined와 false를 같이 검사했다
         return { page: lastPage.page + 1 };
       },
     },
